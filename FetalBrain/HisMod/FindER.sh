be0b93f95d2fda742b0791ad46c4392a5932954f
Gloria Li <glorialuolanli@gmail.com>
2014-05-20 23:20:20 -0700

    Fix a bug with FindER chromosome file input.

--- FetalBrain/HisMod/FindER.sh
+++ FetalBrain/HisMod/FindER.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cd /home/lli/FetalBrain/HisMod/FindER
-chr=/home/mbilenky/UCSC_chr/hg19.chrom.sizes
+chr=/home/mbilenky/UCSC_chr/hg19_auto_XY.chrom.sizes
 map=/projects/epigenomics/resources/UCSC_hg19/mappability/mappability_lt_1
 out=/home/lli/FetalBrain/HisMod/FindER/
 
 inputWig=/home/lli/FetalBrain/HisMod/wigs/A03486.bam.q5.F1028.SET_195.wig.gz
 name=A03486_brain01_H3K4me3
-java -jar -Xmx30G /home/mbilenky/bin/Solexa_Java/FindER.jar -chr $chr -i $inputWig -o $out -n $name -v -m $map -print > A03486_brain01_H3K4me3.FindER_scan.log
+java -jar -Xmx30G /home/mbilenky/bin/Solexa_Java/FindER.jar -chr $chr -i $inputWig -o $out -n $name -v -m $map -print
 
