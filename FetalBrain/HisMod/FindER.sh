d45a06344cde18db5332bd413e1a2da5691368f5
Gloria Li <glorialuolanli@gmail.com>
2014-05-21 10:38:47 -0700

    Run FindER on all FetalBrain histone modification libraries.

--- FetalBrain/HisMod/FindER.sh
+++ FetalBrain/HisMod/FindER.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-cd /home/lli/FetalBrain/HisMod/FindER
+cd /home/lli/FetalBrain/HisMod/wigs/
 chr=/home/mbilenky/UCSC_chr/hg19_auto_XY.chrom.sizes
 map=/projects/epigenomics/resources/UCSC_hg19/mappability/mappability_lt_1
 out=/home/lli/FetalBrain/HisMod/FindER/
 
-inputWig=/home/lli/FetalBrain/HisMod/wigs/A03486.bam.q5.F1028.SET_195.wig.gz
-name=A03486_brain01_H3K4me3
-java -jar -Xmx30G /home/mbilenky/bin/Solexa_Java/FindER.jar -chr $chr -i $inputWig -o $out -n $name -v -m $map -print
-
+for inputWig in *.wig.gz
+do
+    echo "Processing $inputWig"
+    name=`echo $inputWig | cut -d'.' -f 1`
+    java -jar -Xmx30G /home/mbilenky/bin/Solexa_Java/FindER.jar -chr $chr -i $inputWig -o $out -n $name -v -m $map -print > /home/lli/FetalBrain/HisMod/FindER/$name.log
+done
