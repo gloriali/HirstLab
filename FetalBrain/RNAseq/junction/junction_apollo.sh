d6c051bf4859c9910f566f771e1e8cac5fa0351c
Gloria Li <glorialuolanli@gmail.com>
2014-05-23 15:24:08 -0700

    Fix a bug in junction extraction for strand-specific libraries.

--- FetalBrain/RNAseq/junction/junction_apollo.sh
+++ FetalBrain/RNAseq/junction/junction_apollo.sh
@@ -11,7 +11,7 @@ bam=/projects/epigenomics/ep50/internal/jqc.1.7.6/A03484/A03484.bam
 /home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out
 
 bam=/projects/epigenomics/ep50/internal/jqc.1.7.6/A07825/A07825.bam
-/home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out
+/home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out S
 
 bam=/projects/epigenomics/ep50/internal/jqc.1.7.6/A03473/A03473.bam
 /home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out
@@ -23,7 +23,7 @@ bam=/projects/epigenomics/ep50/internal/jqc.1.7.6/A04599/A04599.bam
 /home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out
 
 bam=/projects/epigenomics/ep50/internal/jqc.1.7.6/A15298/A15298.bam
-/home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out
+/home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out S
 
 bam=/projects/epigenomics/ep50/internal/jqc.1.7.6/A03474/A03474.bam
 /home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out
@@ -32,8 +32,8 @@ bam=/projects/epigenomics/ep50/internal/jqc.1.7.6/A03476/A03476.bam
 /home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out
 
 bam=/projects/epigenomics/ep50/internal/jqc.1.7.6/A15295/A15295.bam
-/home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out
+/home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out S
 
 bam=/projects/epigenomics/ep50/internal/jqc.1.7.6/A15299/A15299.bam
-/home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out
+/home/mbilenky/bin/Solexa_Shell/RunJunctions.sh $bam $out S
 
