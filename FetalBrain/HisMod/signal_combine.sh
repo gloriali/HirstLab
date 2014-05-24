fc559cc6479851040b8c552389c25a8b6b42d722
Gloria Li <glorialuolanli@gmail.com>
2014-05-23 22:28:51 -0700

    Separate pc and nc genes for promoter regions.

--- FetalBrain/HisMod/signal_combine.sh
+++ FetalBrain/HisMod/signal_combine.sh
@@ -13,3 +13,17 @@ paste hg19v65_genes_body.pc.A03489.bam.q5.F1028.SET_175.coverage hg19v65_genes_b
 paste hg19v65_genes_body.nc.A03489.bam.q5.F1028.SET_175.coverage hg19v65_genes_body.nc.A03497.bam.q5.F1028.SET_233.coverage hg19v65_genes_body.nc.A03273.bam.q5.F1028.SET_219.coverage hg19v65_genes_body.nc.A03285.bam.q5.F1028.SET_238.coverage hg19v65_genes_body.nc.A03279.bam.q5.F1028.SET_229.coverage hg19v65_genes_body.nc.A03481.bam.q5.F1028.SET_215.coverage | awk '{print $4"\t"$5"\t"$11"\t"$17"\t"$23"\t"$29"\t"$35}' > hg19v65_genes_body.H3K36me3.nc
 # input: promoter (brain01 brain02 cortex02 GE02)
 paste hg19v65_genes_TSS_2000.A03491.bam.q5.F1028.SET_176.coverage hg19v65_genes_TSS_2000.A03499.bam.q5.F1028.SET_130.coverage hg19v65_genes_TSS_2000.A03287.bam.q5.F1028.SET_194.coverage hg19v65_genes_TSS_2000.A03289.bam.q5.F1028.SET_159.coverage hg19v65_genes_TSS_2000.A03288.bam.q5.F1028.SET_174.coverage hg19v65_genes_TSS_2000.A03483.bam.q5.F1028.SET_143.coverage | awk '{print $4"\t"$6"\t"$13"\t"$20"\t"$27"\t"$34"\t"$41}' > hg19v65_genes_TSS_2000.input
+
+# separate pc and nc genes for promoter signal
+less hg19v65_genes_TSS_2000.H3K4me1 | awk '/protein_coding/ {print $0}' > hg19v65_genes_TSS_2000.H3K4me1.pc
+less hg19v65_genes_TSS_2000.H3K4me1 | awk '!/protein_coding/ {print $0}' > hg19v65_genes_TSS_2000.H3K4me1.nc
+rm hg19v65_genes_TSS_2000.H3K4me1
+less hg19v65_genes_TSS_2000.H3K4me3 | awk '/protein_coding/ {print $0}' > hg19v65_genes_TSS_2000.H3K4me3.pc
+less hg19v65_genes_TSS_2000.H3K4me3 | awk '!/protein_coding/ {print $0}' > hg19v65_genes_TSS_2000.H3K4me3.nc
+rm hg19v65_genes_TSS_2000.H3K4me3
+less hg19v65_genes_TSS_2000.H3K27me3 | awk '/protein_coding/ {print $0}' > hg19v65_genes_TSS_2000.H3K27me3.pc
+less hg19v65_genes_TSS_2000.H3K27me3 | awk '!/protein_coding/ {print $0}' > hg19v65_genes_TSS_2000.H3K27me3.nc
+rm hg19v65_genes_TSS_2000.H3K27me3
+less hg19v65_genes_TSS_2000.input | awk '/protein_coding/ {print $0}' > hg19v65_genes_TSS_2000.input.pc
+less hg19v65_genes_TSS_2000.input | awk '!/protein_coding/ {print $0}' > hg19v65_genes_TSS_2000.input.nc
+rm hg19v65_genes_TSS_2000.input
