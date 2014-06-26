b5d27cb1441b3bb6390798ec14aeb9b9363aa959
Gloria Li <glorialuolanli@gmail.com>
2014-06-25 23:01:04 -0700

    Add myo vs H1 isoforms.

--- Breast/isoform/isoform.H1.m
+++ Breast/isoform/isoform.H1.m
@@ -2,6 +2,62 @@
 
 addpath /home/lli/bin/matlab/ -end
 
+% DE exons
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all; close all;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dirIn1='/projects/epigenomics/ep50/internal/jqc.1.7.6/'; 
+dirIn2='/projects/epigenomics/ep50/external/jqc.1.7.6/'; 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+out=true; corr1=true; corr2=true; rpkm=true;figs=true; RPKMmin=0.005; Nmin=25;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fdr=0.015
+[idl,gl]=textread('/projects/epigenomics/resources/Ensembl/hg19v65/hg19v65_exons_for_genes.length','%s %f');
+[idgc,gc]=textread('/projects/epigenomics/resources/Ensembl/hg19v65/hg19v65_exons_for_genes.GC','%s %f');
+
+dirOut='/home/lli/REMC/breast/H1/exon/';
+lib1='A17919'; cell1='myo', donor1='RM084';
+lib2='H1_r1a'; cell2='H1', donor2='H1_r1a';
+
+[coord,geneid,n1,r1]=textread(strcat(dirIn1,lib1,'/coverage/',lib1,'.G.exn.A.rpkm'),'%s %s %f %f');
+[coord,geneid,n2,r2]=textread(strcat(dirIn2,lib2,'/coverage/',lib2,'.G.exn.A.rpkm'),'%s %s %f %f');
+[id]=[strcat(coord,'_',geneid)]
+
+X=intersect(id,intersect(idgc,idl))
+[C,ix,i]=intersect(id,X);
+[C,ixl,i]=intersect(idl,X);
+[C,ixgc,i]=intersect(idgc,X);
+[cc,nfup,nfdn]=DEfine(idl(ixl), r1(ix), r2(ix), n1(ix), n2(ix), [gl(ixl),gc(ixgc)], dirOut, [cell1,'-',donor1], [cell2,'-',donor2], out,figs, fdr,corr1,corr2,rpkm,RPKMmin,Nmin);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all; close all;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dirIn1='/projects/epigenomics/ep50/internal/jqc.1.7.6/'; 
+dirIn2='/projects/epigenomics/ep50/external/jqc.1.7.6/'; 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+out=true; corr1=true; corr2=true; rpkm=true;figs=true; RPKMmin=0.005; Nmin=25;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fdr=0.015
+[idl,gl]=textread('/projects/epigenomics/resources/Ensembl/hg19v65/hg19v65_exons_for_genes.length','%s %f');
+[idgc,gc]=textread('/projects/epigenomics/resources/Ensembl/hg19v65/hg19v65_exons_for_genes.GC','%s %f');
+
+dirOut='/home/lli/REMC/breast/H1/exon/';
+lib1='A17919'; cell1='myo', donor1='RM084';
+lib2='H1_r2a'; cell2='H1', donor2='H1_r2a';
+
+[coord,geneid,n1,r1]=textread(strcat(dirIn1,lib1,'/coverage/',lib1,'.G.exn.A.rpkm'),'%s %s %f %f');
+[coord,geneid,n2,r2]=textread(strcat(dirIn2,lib2,'/coverage/',lib2,'.G.exn.A.rpkm'),'%s %s %f %f');
+[id]=[strcat(coord,'_',geneid)]
+
+X=intersect(id,intersect(idgc,idl))
+[C,ix,i]=intersect(id,X);
+[C,ixl,i]=intersect(idl,X);
+[C,ixgc,i]=intersect(idgc,X);
+[cc,nfup,nfdn]=DEfine(idl(ixl), r1(ix), r2(ix), n1(ix), n2(ix), [gl(ixl),gc(ixgc)], dirOut, [cell1,'-',donor1], [cell2,'-',donor2], out,figs, fdr,corr1,corr2,rpkm,RPKMmin,Nmin);
+
+% DE genes 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all; close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
