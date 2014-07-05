6b0d80487e4ebc8f63c1d860c2ab94f5cecb9edc
Gloria Li <glorialuolanli@gmail.com>
2014-07-05 14:53:24 -0700

    Add enrich R function for plotting functional enrichment terms.

--- Pipeline/README.md
+++ Pipeline/README.md
@@ -6,6 +6,7 @@ Analysis pipeline
 * [ChromHMM.md](./ChromHMM.md): instructions on running ChromHMM. _unfinished_.  
 * [DEfine.m](./DEfine.m): copy of Misha's DEfine function.   
 * [myfnorm.m](./myfnorm.m): copy of Misha's myfnorm function, required for DEfine.
+* [enrich.R](./enrich.R): enrich function to plot enriched terms with `ggplot2`.   
 * [isoform.R](isoform.R): isoform R function for identifying isoform genes and cassete exons.
 * [epiProfile.R](./epiProfile.R): epiProfile R function for calculating and plotting epigenetic profiles for isoform and intron retention.       
 * [UCSC](./UCSC): script from UCSC genome browser and sample code for building track hub.
---
+++ Pipeline/enrich.R
@@ -0,0 +1,42 @@
+enrich <- function(name, dirIn = "./enrich/", dirOut = "./enrich/", fdr = 0.01, category = c("GOBP", "GOMF", "KEGG_PATHWAY", "PANTHER_PATHWAY", "REACTOME_PATHWAY", "INTERPRO"), height = 6, width = 9){
+  library(ggplot2)
+  DAVID <- read.delim(paste0(dirIn, name, "_DAVID.txt"), as.is=T)
+  erminej_BP <- read.delim(paste0(dirIn, name, "_erminej_BP.txt"), as.is=T, skip = 34, head = F)
+  if(sum(is.na(erminej_BP$V10)) == nrow(erminej_BP)){erminej_BP$V10 <- erminej_BP$V8}
+  erminej_MF <- read.delim(paste0(dirIn, name, "_erminej_MF.txt"), as.is=T, skip = 34, head = F)
+  if(sum(is.na(erminej_MF$V10)) == nrow(erminej_MF)){erminej_MF$V10 <- erminej_MF$V8}
+  enrich <- na.omit(rbind(data.frame(Category = "GOBP", Term = erminej_BP$V2, FDR = erminej_BP$V10), data.frame(Category = "GOMF", Term = erminej_MF$V2, FDR = erminej_MF$V10), data.frame(Category = DAVID$Category, Term = DAVID$Term, FDR = DAVID$FDR)))
+  enrich <- enrich[enrich$FDR <= fdr, ]
+  enrich <- enrich[as.character(enrich$Category) %in% category, ]
+  enrich$Term <- gsub("IPR[0-9]+:", "", enrich$Term)
+  enrich$Term <- gsub("hsa[0-9]+:", "", enrich$Term)
+  enrich$Term <- gsub("P[0-9]+:", "", enrich$Term)
+  enrich$Term <- gsub("REACT_[0-9]+:", "", enrich$Term)
+  enrich$Term <- strtrim(enrich$Term, 50)
+  enrich$Term <- factor(enrich$Term, levels = enrich[order(enrich$Category, enrich$FDR, decreasing = T),]$Term)
+  Enrich_plot <- ggplot(data = enrich, aes(Term, -log10(FDR))) +
+     geom_bar(aes(fill = Category), width = .5) + 
+     coord_flip() + 
+     geom_text(aes(label = round(-log10(FDR), 2), hjust = 0)) + 
+     theme_bw() +
+     ggtitle(paste0("Functional enrichment for ", name)) + 
+     scale_fill_manual(values = c("GOBP" = "blue", "GOMF" = "purple", "KEGG_PATHWAY" = "darkgreen", "PANTHER_PATHWAY" = "darkblue", "REACTOME_PATHWAY" = "steelblue", "INTERPRO" = "lightblue"))
+  ggsave(Enrich_plot, file = paste0(dirOut, name, "_enrich.pdf"), height = height, width = width)
+  return(Enrich_plot)
+}
+# Plot enrichment terms from ermineJ (GO) and DAVID (KEGG, PANTHER, REACTOME, INTERPRO)
+# Criteria: 
+#   ermineJ (GO) results: <name>_erminej_BP.txt and <name>_erminej_MF.txt
+#   DAVID (KEGG, PANTHER, REACTOME, INTERPRO) result: <name>_DAVID.txt
+# Parameters: 
+#   name: file name prefix 
+#   dirIn: directory with ermineJ and DAVID results, default to <current wd>/enrich/ 
+#   dirOut: output directory, default to <current wd>/enrich/
+#   fdr: FDR cutoff for enriched terms
+#   category: categories of terms to include, default to `c("GOBP", "GOMF", "KEGG_PATHWAY", "PANTHER_PATHWAY", "REACTOME_PATHWAY", "INTERPRO")`
+#   height, width: for output pdf plot, default to 6*9
+# Output: 
+#   pdf figure with enrichment plot
+#   return ggplot2 object for enrichment plot
+
+
