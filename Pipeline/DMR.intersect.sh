ba991f7769c4c7cba48e64de42ceadcc401a4210
Gloria Li <glorialuolanli@gmail.com>
2014-09-11 12:23:26 -0700

    Add intersect with pc genes and promoters.

--- Pipeline/DMR.intersect.sh
+++ Pipeline/DMR.intersect.sh
@@ -12,19 +12,27 @@ dirOut=$dirIn/CpG
 mkdir -p $dirOut
 > $dirOut/genomic.breakdown.summary
 cd $dirIn
-echo -e "Intersecting DM CpGs with genomic regions\nOutput directory: $dirOut\nOutput DM CpG percentage to genomic.breakdown.summary\ngenomic.breakdown.summary format: Name\tTotal No. of DM CpGs\tIntergenic\tIntron\tExon\tGene\tPromoter"
+echo -e "Intersecting DM CpGs with genomic regions
+Output files: $dirOut/DMR.$name.CpG.bed: chr\tstart\tend\tDMR_ID>DM
+$dirOut/DMR.$name.CpG_gene.bed: chr\tstart\tend\tDMR_ID>DM\tEnsembl
+$dirOut/DMR.$name.CpG_exon.bed: chr\tstart\tend\tDMR_ID>DM\tExon_ID
+$dirOut/DMR.$name.CpG_promoter.bed: chr\tstart\tend\tDMR_ID>DM\tEnsembl
+$dirOut/DMR.$name.CpG_gene_pc.bed: chr\tstart\tend\tDMR_ID>DM\tEnsembl
+$dirOut/DMR.$name.CpG_gene_promoter_pc.bed: chr\tstart\tend\tDMR_ID>DM\tEnsembl
+$dirOut/genomic.breakdown.summary: Name\tTotal No. of DM CpGs\tIntergenic\tIntron\tExon\tGene\tPromoter"\n
 for dmr in DMR.*.bed
 do
     name=`echo $dmr | cut -d'.' -f 2`
     echo "Processing $name" 
-    /gsc/software/linux-x86_64-centos5/bedtools-2.17.0/bin/intersectBed -a /home/lli/hg19/CG.BED -b $dmr -wa -wb | awk '{print $1"\t"$2"\t"$3"\t"$1":"$2"-"$3"\t"$7}' > $dirOut/DMR.$name.CpG.bed
+    /gsc/software/linux-x86_64-centos5/bedtools-2.17.0/bin/intersectBed -a /home/lli/hg19/CG.BED -b $dmr -wa -wb | awk '{print $1"\t"$2"\t"$3"\t"$7">"$8}' > $dirOut/DMR.$name.CpG.bed
     /gsc/software/linux-x86_64-centos5/bedtools-2.17.0/bin/intersectBed -a $dirOut/DMR.$name.CpG.bed -b /home/lli/hg19/hg19v65_genes.bed -wa -wb | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$8}' > $dirOut/DMR.$name.CpG_gene.bed
     /gsc/software/linux-x86_64-centos5/bedtools-2.17.0/bin/intersectBed -a $dirOut/DMR.$name.CpG.bed -b /home/lli/hg19/hg19v65_exons.bed -wa -wb | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$5":"$6"-"$7}' > $dirOut/DMR.$name.CpG_exon.bed
     /gsc/software/linux-x86_64-centos5/bedtools-2.17.0/bin/intersectBed -a $dirOut/DMR.$name.CpG.bed -b /home/lli/hg19/hg19v65_genes_TSS_1500.bed -wa -wb | awk '{print $1"\t"$2"\t"$3"\t"$4"\t"$8}' > $dirOut/DMR.$name.CpG_promoter.bed
+    less $dirOut/DMR.$name.CpG_gene.bed | awk '/protein_coding/ {gsub("_protein_coding", "", $5); print $1"\t"$2"\t"$3"\t"$4"\t"$5}' > $dirOut/DMR.$name.CpG_gene_pc.bed
+    less $dirOut/DMR.$name.CpG_promoter.bed | awk '/protein_coding/ {gsub("_protein_coding", "", $5); print $1"\t"$2"\t"$3"\t"$4"\t"$5}' > $dirOut/DMR.$name.CpG_promoter_pc.bed
     total=`wc -l $dirOut/DMR.$name.CpG.bed | cut -d' ' -f 1`
     gene=`wc -l $dirOut/DMR.$name.CpG_gene.bed | cut -d' ' -f 1`
     exon=`wc -l $dirOut/DMR.$name.CpG_exon.bed | cut -d' ' -f 1`
-    promoter=`wc -l $dirOut/DMR.$name.CpG_promoter.bed | cut -d' ' -f 1`
     echo -e "$name\t$total\t$gene\t$exon\t$promoter" | awk '{print $1"\t"$2"\t"($2-$3)/$2"\t"($3-$4)/$2"\t"$4/$2"\t"$3/$2"\t"$5/$2}' >> $dirOut/genomic.breakdown.summary
 done
 
