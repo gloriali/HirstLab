65040ec8827886b262a729528a75cf6ce940d6fa
Gloria Li <glorialuolanli@gmail.com>
2014-09-19 17:55:23 -0700

    Modify MeDIP DMR pipeline to report both DMR stats and DM CpG stats.

--- FetalBrain/MeDIP/MeDIP.DM.sh
+++ FetalBrain/MeDIP/MeDIP.DM.sh
@@ -20,9 +20,9 @@ cut=3     # minimum number of CpGs
 lib1="HS2777"; cell1="GE"; donor1="HuFNSC01"; name1="HS2777.MeDIP.NeurospheresGE01.q5.F1028.SET_157";
 lib2="HS2781"; cell2="GE"; donor2="HuFNSC02"; name2="HS2781.MeDIP.NeurospheresGE02.q5.F1028.SET_166";
 dm=DM.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.bed 
-#echo -e DMRs between $cell1"-"$donor1 and $cell2"-"$donor2: delta=$delta, size=$size, cut=$cut, output: chr"\t"start"\t"end"\t"ID"\t"DM"\t"No.of CpGs"\t"length
-#paste $dirIn/$name1.dip $dirIn/$name2.dip | awk -v delta=$delta '{if($1!=$3)print "Bad line", $0; chr="chr"gensub("_[0-9]+", "", "g", $1); start=gensub("[0-9XY]+_", "", "g", $1)+23; end=start+2; if($2-$4 > delta)print chr"\t"start"\t"end"\t1\t"$2"\t"$4; else if($2-$4 < -delta)print chr"\t"start"\t"end"\t-1\t"$2"\t"$4}' | sort -k1,1 -k2,2n > $dirDM/$dm
-#less $dirDM/$dm | grep 'Bad line'
+echo -e DMRs between $cell1"-"$donor1 and $cell2"-"$donor2: delta=$delta, size=$size, cut=$cut, output: chr"\t"start"\t"end"\t"ID"\t"DM"\t"No.of CpGs"\t"length
+paste $dirIn/$name1.dip $dirIn/$name2.dip | awk -v delta=$delta '{if($1!=$3)print "Bad line", $0; chr="chr"gensub("_[0-9]+", "", "g", $1); start=gensub("[0-9XY]+_", "", "g", $1)+23; end=start+2; if($2-$4 > delta)print chr"\t"start"\t"end"\t1\t"$2"\t"$4; else if($2-$4 < -delta)print chr"\t"start"\t"end"\t-1\t"$2"\t"$4}' | sort -k1,1 -k2,2n > $dirDM/$dm
+less $dirDM/$dm | grep 'Bad line'
 for size in 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500
 do
     less $dirDM/$dm | awk 'BEGIN{size="'$size'"+0; cut="'$cut'"+0} {if($2<end+size && $4==dm && $1==chr){end=$3;c=c+1} else {if(end!=null){if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}; chr=$1;start=$2;end=$3;c=1;dm=$4}}END{if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}' > $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut
@@ -44,6 +44,7 @@ dirIn='/projects/epigenomics/users/lli/FetalBrain/MeDIP'
 dirDM=$dirIn/DMR
 mkdir -p $dirDM
 >$dirDM/DMR.summary.stats # samples, delta, size, cut, Average length of DMRs, Average No.of CpGs per DMR, No.of DMRs, No.of hypermethylated DMRs, No.of hypomethylated DMRs    
+>$dirDM/DM.summary.stats # samples, delta, No.of DM CpGs, No.of hypermethylated DM CpGs, No.of hypomethylated DM CpGs    
 
 ## Brain01 vs Brain02
 lib1="HS2788"; cell1="Brain"; donor1="HuFNSC01"; name1="HS2788.MeDIP.Brain01.q5.F1028.SET_174";
@@ -52,8 +53,17 @@ dm=DM.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.bed
 echo -e DMRs between $cell1"-"$donor1 and $cell2"-"$donor2: delta=$delta, size=$size, cut=$cut, output: chr"\t"start"\t"end"\t"ID"\t"DM"\t"No.of CpGs"\t"length
 paste $dirIn/$name1.dip $dirIn/$name2.dip | awk -v delta=$delta '{if($1!=$3)print "Bad line", $0; chr="chr"gensub("_[0-9]+", "", "g", $1); start=gensub("[0-9XY]+_", "", "g", $1)+23; end=start+2; if($2-$4 > delta)print chr"\t"start"\t"end"\t1\t"$2"\t"$4; else if($2-$4 < -delta)print chr"\t"start"\t"end"\t-1\t"$2"\t"$4}' | sort -k1,1 -k2,2n > $dirDM/$dm
 less $dirDM/$dm | grep 'Bad line'
+Ndm=($(wc -l $dirDM/$dm))
+Nhyper=($(less $dirDM/$dm | awk '{if($4==1){c=c+1}} END{print c}'))
+Nhypo=($(less $dirDM/$dm | awk '{if($4==-1){c=c+1}} END{print c}'))
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$Ndm"\t"$Nhyper"\t"$Nhypo >> $dirDM/DM.summary.stats
 less $dirDM/$dm | awk 'BEGIN{size="'$size'"+0; cut="'$cut'"+0} {if($2<end+size && $4==dm && $1==chr){end=$3;c=c+1} else {if(end!=null){if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}; chr=$1;start=$2;end=$3;c=1;dm=$4}}END{if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}' > $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut
-less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{l=l+$7; c=c+$6; count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print "'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'""\t""'$delta'""\t""'$size'""\t""'$cut'""\t"l/count"\t"c/count"\t"count"\t"hyper"\t"count-hyper}' >> $dirDM/DMR.summary.stats
+dmr=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print count}'))
+hyper=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hyper))
+hypo=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hypo))
+length=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k7,7n | awk '{len[NR]=$7} END{if(NR%2){print len[(NR+1)/2]} else{print (len[(NR/2)]+len[(NR/2)+1])/2}}'))
+count=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k6,6n | awk '{count[NR]=$6} END{if(NR%2){print count[(NR+1)/2]} else{print (count[(NR/2)]+count[(NR/2)+1])/2}}')) 
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$size"\t"$cut"\t"$length"\t"$count"\t"$dmr"\t"$hyper"\t"$hypo >> $dirDM/DMR.summary.stats.test
 
 ## Cortex01 vs Cortex02
 lib1="HS2775"; cell1="Cortex"; donor1="HuFNSC01"; name1="HS2775.MeDIP.NeurospheresCortex01.q5.F1028.SET_174";
@@ -62,8 +72,17 @@ dm=DM.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.bed
 echo -e DMRs between $cell1"-"$donor1 and $cell2"-"$donor2: delta=$delta, size=$size, cut=$cut, output: chr"\t"start"\t"end"\t"ID"\t"DM"\t"No.of CpGs"\t"length
 paste $dirIn/$name1.dip $dirIn/$name2.dip | awk -v delta=$delta '{if($1!=$3)print "Bad line", $0; chr="chr"gensub("_[0-9]+", "", "g", $1); start=gensub("[0-9XY]+_", "", "g", $1)+23; end=start+2; if($2-$4 > delta)print chr"\t"start"\t"end"\t1\t"$2"\t"$4; else if($2-$4 < -delta)print chr"\t"start"\t"end"\t-1\t"$2"\t"$4}' | sort -k1,1 -k2,2n > $dirDM/$dm
 less $dirDM/$dm | grep 'Bad line'
+Ndm=($(wc -l $dirDM/$dm))
+Nhyper=($(less $dirDM/$dm | awk '{if($4==1){c=c+1}} END{print c}'))
+Nhypo=($(less $dirDM/$dm | awk '{if($4==-1){c=c+1}} END{print c}'))
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$Ndm"\t"$Nhyper"\t"$Nhypo >> $dirDM/DM.summary.stats
 less $dirDM/$dm | awk 'BEGIN{size="'$size'"+0; cut="'$cut'"+0} {if($2<end+size && $4==dm && $1==chr){end=$3;c=c+1} else {if(end!=null){if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}; chr=$1;start=$2;end=$3;c=1;dm=$4}}END{if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}' > $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut
-less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{l=l+$7; c=c+$6; count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print "'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'""\t""'$delta'""\t""'$size'""\t""'$cut'""\t"l/count"\t"c/count"\t"count"\t"hyper"\t"count-hyper}' >> $dirDM/DMR.summary.stats
+dmr=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print count}'))
+hyper=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hyper))
+hypo=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hypo))
+length=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k7,7n | awk '{len[NR]=$7} END{if(NR%2){print len[(NR+1)/2]} else{print (len[(NR/2)]+len[(NR/2)+1])/2}}'))
+count=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k6,6n | awk '{count[NR]=$6} END{if(NR%2){print count[(NR+1)/2]} else{print (count[(NR/2)]+count[(NR/2)+1])/2}}')) 
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$size"\t"$cut"\t"$length"\t"$count"\t"$dmr"\t"$hyper"\t"$hypo >> $dirDM/DMR.summary.stats.test
 
 ## GE01 vs GE02
 lib1="HS2777"; cell1="GE"; donor1="HuFNSC01"; name1="HS2777.MeDIP.NeurospheresGE01.q5.F1028.SET_157";
@@ -72,8 +91,17 @@ dm=DM.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.bed
 echo -e DMRs between $cell1"-"$donor1 and $cell2"-"$donor2: delta=$delta, size=$size, cut=$cut, output: chr"\t"start"\t"end"\t"ID"\t"DM"\t"No.of CpGs"\t"length
 paste $dirIn/$name1.dip $dirIn/$name2.dip | awk -v delta=$delta '{if($1!=$3)print "Bad line", $0; chr="chr"gensub("_[0-9]+", "", "g", $1); start=gensub("[0-9XY]+_", "", "g", $1)+23; end=start+2; if($2-$4 > delta)print chr"\t"start"\t"end"\t1\t"$2"\t"$4; else if($2-$4 < -delta)print chr"\t"start"\t"end"\t-1\t"$2"\t"$4}' | sort -k1,1 -k2,2n > $dirDM/$dm
 less $dirDM/$dm | grep 'Bad line'
+Ndm=($(wc -l $dirDM/$dm))
+Nhyper=($(less $dirDM/$dm | awk '{if($4==1){c=c+1}} END{print c}'))
+Nhypo=($(less $dirDM/$dm | awk '{if($4==-1){c=c+1}} END{print c}'))
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$Ndm"\t"$Nhyper"\t"$Nhypo >> $dirDM/DM.summary.stats
 less $dirDM/$dm | awk 'BEGIN{size="'$size'"+0; cut="'$cut'"+0} {if($2<end+size && $4==dm && $1==chr){end=$3;c=c+1} else {if(end!=null){if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}; chr=$1;start=$2;end=$3;c=1;dm=$4}}END{if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}' > $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut
-less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{l=l+$7; c=c+$6; count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print "'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'""\t""'$delta'""\t""'$size'""\t""'$cut'""\t"l/count"\t"c/count"\t"count"\t"hyper"\t"count-hyper}' >> $dirDM/DMR.summary.stats
+dmr=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print count}'))
+hyper=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hyper))
+hypo=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hypo))
+length=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k7,7n | awk '{len[NR]=$7} END{if(NR%2){print len[(NR+1)/2]} else{print (len[(NR/2)]+len[(NR/2)+1])/2}}'))
+count=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k6,6n | awk '{count[NR]=$6} END{if(NR%2){print count[(NR+1)/2]} else{print (count[(NR/2)]+count[(NR/2)+1])/2}}')) 
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$size"\t"$cut"\t"$length"\t"$count"\t"$dmr"\t"$hyper"\t"$hypo >> $dirDM/DMR.summary.stats.test
 
 ## Cortex01 vs GE01
 lib1="HS2775"; cell1="Cortex"; donor1="HuFNSC01"; name1="HS2775.MeDIP.NeurospheresCortex01.q5.F1028.SET_174";
@@ -82,8 +110,17 @@ dm=DM.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.bed
 echo -e DMRs between $cell1"-"$donor1 and $cell2"-"$donor2: delta=$delta, size=$size, cut=$cut, output: chr"\t"start"\t"end"\t"ID"\t"DM"\t"No.of CpGs"\t"length
 paste $dirIn/$name1.dip $dirIn/$name2.dip | awk -v delta=$delta '{if($1!=$3)print "Bad line", $0; chr="chr"gensub("_[0-9]+", "", "g", $1); start=gensub("[0-9XY]+_", "", "g", $1)+23; end=start+2; if($2-$4 > delta)print chr"\t"start"\t"end"\t1\t"$2"\t"$4; else if($2-$4 < -delta)print chr"\t"start"\t"end"\t-1\t"$2"\t"$4}' | sort -k1,1 -k2,2n > $dirDM/$dm
 less $dirDM/$dm | grep 'Bad line'
+Ndm=($(wc -l $dirDM/$dm))
+Nhyper=($(less $dirDM/$dm | awk '{if($4==1){c=c+1}} END{print c}'))
+Nhypo=($(less $dirDM/$dm | awk '{if($4==-1){c=c+1}} END{print c}'))
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$Ndm"\t"$Nhyper"\t"$Nhypo >> $dirDM/DM.summary.stats
 less $dirDM/$dm | awk 'BEGIN{size="'$size'"+0; cut="'$cut'"+0} {if($2<end+size && $4==dm && $1==chr){end=$3;c=c+1} else {if(end!=null){if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}; chr=$1;start=$2;end=$3;c=1;dm=$4}}END{if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}' > $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut
-less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{l=l+$7; c=c+$6; count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print "'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'""\t""'$delta'""\t""'$size'""\t""'$cut'""\t"l/count"\t"c/count"\t"count"\t"hyper"\t"count-hyper}' >> $dirDM/DMR.summary.stats
+dmr=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print count}'))
+hyper=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hyper))
+hypo=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hypo))
+length=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k7,7n | awk '{len[NR]=$7} END{if(NR%2){print len[(NR+1)/2]} else{print (len[(NR/2)]+len[(NR/2)+1])/2}}'))
+count=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k6,6n | awk '{count[NR]=$6} END{if(NR%2){print count[(NR+1)/2]} else{print (count[(NR/2)]+count[(NR/2)+1])/2}}')) 
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$size"\t"$cut"\t"$length"\t"$count"\t"$dmr"\t"$hyper"\t"$hypo >> $dirDM/DMR.summary.stats.test
 
 ## Cortex02 vs GE02
 lib1="HS2779"; cell1="Cortex"; donor1="HuFNSC02"; name1="HS2779.MeDIP.NeurospheresCortex02.q5.F1028.SET_174";
@@ -92,6 +129,15 @@ dm=DM.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.bed
 echo -e DMRs between $cell1"-"$donor1 and $cell2"-"$donor2: delta=$delta, size=$size, cut=$cut, output: chr"\t"start"\t"end"\t"ID"\t"DM"\t"No.of CpGs"\t"length
 paste $dirIn/$name1.dip $dirIn/$name2.dip | awk -v delta=$delta '{if($1!=$3)print "Bad line", $0; chr="chr"gensub("_[0-9]+", "", "g", $1); start=gensub("[0-9XY]+_", "", "g", $1)+23; end=start+2; if($2-$4 > delta)print chr"\t"start"\t"end"\t1\t"$2"\t"$4; else if($2-$4 < -delta)print chr"\t"start"\t"end"\t-1\t"$2"\t"$4}' | sort -k1,1 -k2,2n > $dirDM/$dm
 less $dirDM/$dm | grep 'Bad line'
+Ndm=($(wc -l $dirDM/$dm))
+Nhyper=($(less $dirDM/$dm | awk '{if($4==1){c=c+1}} END{print c}'))
+Nhypo=($(less $dirDM/$dm | awk '{if($4==-1){c=c+1}} END{print c}'))
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$Ndm"\t"$Nhyper"\t"$Nhypo >> $dirDM/DM.summary.stats
 less $dirDM/$dm | awk 'BEGIN{size="'$size'"+0; cut="'$cut'"+0} {if($2<end+size && $4==dm && $1==chr){end=$3;c=c+1} else {if(end!=null){if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}; chr=$1;start=$2;end=$3;c=1;dm=$4}}END{if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}' > $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut
-less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{l=l+$7; c=c+$6; count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print "'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'""\t""'$delta'""\t""'$size'""\t""'$cut'""\t"l/count"\t"c/count"\t"count"\t"hyper"\t"count-hyper}' >> $dirDM/DMR.summary.stats
+dmr=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print count}'))
+hyper=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hyper))
+hypo=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hypo))
+length=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k7,7n | awk '{len[NR]=$7} END{if(NR%2){print len[(NR+1)/2]} else{print (len[(NR/2)]+len[(NR/2)+1])/2}}'))
+count=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k6,6n | awk '{count[NR]=$6} END{if(NR%2){print count[(NR+1)/2]} else{print (count[(NR/2)]+count[(NR/2)+1])/2}}')) 
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$size"\t"$cut"\t"$length"\t"$count"\t"$dmr"\t"$hyper"\t"$hypo >> $dirDM/DMR.summary.stats.test
 
--- Pipeline/MeDIP.DMR.md
+++ Pipeline/MeDIP.DMR.md
@@ -117,14 +117,24 @@ dirIn='/projects/epigenomics/users/lli/FetalBrain/MeDIP'
 dirDM=$dirIn/DMR
 mkdir -p $dirDM
 >$dirDM/DMR.summary.stats # samples, delta, size, cut, Average length of DMRs, Average No.of CpGs per DMR, No.of DMRs, No.of hypermethylated DMRs, No.of hypomethylated DMRs    
+>$dirDM/DM.summary.stats # samples, delta, No.of DM CpGs, No.of hypermethylated DM CpGs, No.of hypomethylated DM CpGs    
 lib1="HS2788"; cell1="Brain"; donor1="HuFNSC01"; name1="HS2788.MeDIP.Brain01.q5.F1028.SET_174";
 lib2="HS2790"; cell2="Brain"; donor2="HuFNSC02"; name2="HS2790.MeDIP.Brain02.q5.F1028.SET_174";
 dm=DM.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.bed 
 echo -e DMRs between $cell1"-"$donor1 and $cell2"-"$donor2: delta=$delta, size=$size, cut=$cut, output: chr"\t"start"\t"end"\t"ID"\t"DM"\t"No.of CpGs"\t"length
 paste $dirIn/$name1.dip $dirIn/$name2.dip | awk -v delta=$delta '{if($1!=$3)print "Bad line", $0; chr="chr"gensub("_[0-9]+", "", "g", $1); start=gensub("[0-9XY]+_", "", "g", $1)+23; end=start+2; if($2-$4 > delta)print chr"\t"start"\t"end"\t1\t"$2"\t"$4; else if($2-$4 < -delta)print chr"\t"start"\t"end"\t-1\t"$2"\t"$4}' | sort -k1,1 -k2,2n > $dirDM/$dm
 less $dirDM/$dm | grep 'Bad line'
+Ndm=($(wc -l $dirDM/$dm))
+Nhyper=($(less $dirDM/$dm | awk '{if($4==1){c=c+1}} END{print c}'))
+Nhypo=($(less $dirDM/$dm | awk '{if($4==-1){c=c+1}} END{print c}'))
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$Ndm"\t"$Nhyper"\t"$Nhypo >> $dirDM/DM.summary.stats
 less $dirDM/$dm | awk 'BEGIN{size="'$size'"+0; cut="'$cut'"+0} {if($2<end+size && $4==dm && $1==chr){end=$3;c=c+1} else {if(end!=null){if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}; chr=$1;start=$2;end=$3;c=1;dm=$4}}END{if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}' > $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut
-less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{l=l+$7; c=c+$6; count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print "'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'""\t""'$delta'""\t""'$size'""\t""'$cut'""\t"l/count"\t"c/count"\t"count"\t"hyper"\t"count-hyper}' >> $dirDM/DMR.summary.stats
+dmr=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | awk '{count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirDM'""/DMR.""'$cell1'""-""'$donor1'""_""'$cell2'""-""'$donor2'"".d""'$delta'"".s""'$size'"".c""'$cut'"".hypo"}} END {print count}'))
+hyper=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hyper))
+hypo=($(wc -l $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut.hypo))
+length=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k7,7n | awk '{len[NR]=$7} END{if(NR%2){print len[(NR+1)/2]} else{print (len[(NR/2)]+len[(NR/2)+1])/2}}'))
+count=($(less $dirDM/DMR.$cell1"-"$donor1"_"$cell2"-"$donor2.d$delta.s$size.c$cut | sort -k6,6n | awk '{count[NR]=$6} END{if(NR%2){print count[(NR+1)/2]} else{print (count[(NR/2)]+count[(NR/2)+1])/2}}')) 
+echo -e $cell1"-"$donor1"_"$cell2"-"$donor2"\t"$delta"\t"$size"\t"$cut"\t"$length"\t"$count"\t"$dmr"\t"$hyper"\t"$hypo >> $dirDM/DMR.summary.stats.test
 ```
 
 
