f1a78a65118ab47f7ea73b52b27ac1f53651eba1
Gloria Li <glorialuolanli@gmail.com>
2014-09-20 20:30:18 -0700

    Correct typos.

--- Pipeline/DMR.dynamic.sh
+++ Pipeline/DMR.dynamic.sh
@@ -16,7 +16,7 @@ do
     shift
 done
 
-echo -e "Processing "$file", output format chr"\t"start"\t"end"\t"ID"\t"(hyper/hypo)"\t"count"\t"length"
+echo -e "Processing "$file", output format chr\tstart\tend\tID\t(hyper/hypo)\tcount\tlength"
 
 less $dirIn/$file | awk 'BEGIN{size="'$size'"+0; cut="'$cut'"+0} {if($2<end+size && $4==dm && $1==chr){end=$3;c=c+1} else {if(end!=null){if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}; chr=$1;start=$2;end=$3;c=1;dm=$4}}END{if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}' > $dirOut/DMR.$name.s$size.c$cut
 dmr=($(less $dirOut/DMR.$name.s$size.c$cut | awk '{count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirOut'""/DMR.""'$name'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirOut'""/DMR.""'$name'"".s""'$size'"".c""'$cut'"".hypo"}} END {print count}'))
@@ -24,5 +24,5 @@ hyper=($(wc -l $dirOut/DMR.$name.s$size.c$cut.hyper))
 hypo=($(wc -l $dirOut/DMR.$name.s$size.c$cut.hypo))
 length=($(less $dirOut/DMR.$name.s$size.c$cut | sort -k7,7n | awk '{len[NR]=$7} END{if(NR%2){print len[(NR+1)/2]} else{print (len[(NR/2)]+len[(NR/2)+1])/2}}'))
 count=($(less $dirOut/DMR.$name.s$size.c$cut | sort -k6,6n | awk '{count[NR]=$6} END{if(NR%2){print count[(NR+1)/2]} else{print (count[(NR/2)]+count[(NR/2)+1])/2}}')) 
-echo -e $name"\t"$size"\t"$cut"\t"$length"\t"$count"\t"$dmr"\t"$hyper"\t"$hypo >> $dirOut/DMR.summary.stats.test
+echo -e $name"\t"$size"\t"$cut"\t"$length"\t"$count"\t"$dmr"\t"$hyper"\t"$hypo >> $dirOut/DMR.summary.stats
 
