d6ea8e10410da0b639082c6659d363a847d3cc34
Gloria Li <glorialuolanli@gmail.com>
2014-09-20 17:36:13 -0700

    Add dynamic growth DMR approach to pipeline.

---
+++ Pipeline/DMR.dynamic.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Dynamic growth appraoch to join DM CpGs
+size=500  # max distance between two consecutive CpGs
+cut=3     # minimum number of CpGs
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -i) dirIn="$2"; shift;;
+        -o) dirOut="$2"; shift;;
+        -f) file="$2"; shift;;
+        -n) name="$2"; shift;;
+        -s) size="$2"; shift;;
+        -c) cut="$2"; shift;;
+    esac
+    shift
+done
+
+echo -e "Processing "$file", output format chr"\t"start"\t"end"\t"ID"\t"(hyper/hypo)"\t"count"\t"length"
+
+less $dirIn/$file | awk 'BEGIN{size="'$size'"+0; cut="'$cut'"+0} {if($2<end+size && $4==dm && $1==chr){end=$3;c=c+1} else {if(end!=null){if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}; chr=$1;start=$2;end=$3;c=1;dm=$4}}END{if(c>cut){l=end-start;print chr"\t"start"\t"end"\t"chr":"start"-"end"\t"dm"\t"c"\t"l}}' > $dirOut/DMR.$name.s$size.c$cut
+dmr=($(less $dirOut/DMR.$name.s$size.c$cut | awk '{count++; if($5==1){hyper++;hyperlen=hyperlen+$7; print $0 >> "'$dirOut'""/DMR.""'$name'"".s""'$size'"".c""'$cut'"".hyper"} else {print $0 >> "'$dirOut'""/DMR.""'$name'"".s""'$size'"".c""'$cut'"".hypo"}} END {print count}'))
+hyper=($(wc -l $dirOut/DMR.$name.s$size.c$cut.hyper))
+hypo=($(wc -l $dirOut/DMR.$name.s$size.c$cut.hypo))
+length=($(less $dirOut/DMR.$name.s$size.c$cut | sort -k7,7n | awk '{len[NR]=$7} END{if(NR%2){print len[(NR+1)/2]} else{print (len[(NR/2)]+len[(NR/2)+1])/2}}'))
+count=($(less $dirOut/DMR.$name.s$size.c$cut | sort -k6,6n | awk '{count[NR]=$6} END{if(NR%2){print count[(NR+1)/2]} else{print (count[(NR/2)]+count[(NR/2)+1])/2}}')) 
+echo -e $name"\t"$size"\t"$cut"\t"$length"\t"$count"\t"$dmr"\t"$hyper"\t"$hypo >> $dirOut/DMR.summary.stats.test
+
--- Pipeline/README.md
+++ Pipeline/README.md
@@ -7,7 +7,10 @@ Analysis pipeline
 * [junction.md](./junction.md): instructions for validating isoforms with junction reads.  
 * [epiProfile.md](./epiProfile.md): instructions for calculating and plotting epigenetic profiles for isoform and intron retention.       
 * [MeDIP.DMR.md](./MeDIP.DMR.md): instructions for DMR analysis from MeDIP fractional methylation calls.        
-* [ChromHMM.md](./ChromHMM.md): instructions on running ChromHMM. _unfinished_.  
+* [ChromHMM.md](./ChromHMM.md): instructions on running ChromHMM. _unfinished_.
+* [DMR.dynamic.sh](./DMR.dynamic.sh): shell script to collapse DM CpGs into DMRs.
+* [WGBS.combine.sh](./WGBS.combine.sh): shell script to combine coverage from both strand for CpGs.
+* [methyl_diff.sh](./methyl_diff.sh): shell script to identify DM CpGs for WGBS using methyl_diff.  
 * [MeDIP.DMR.py](./MeDIP.DMR.py): collapse single DM CpGs to DMRs.
 * [DMR.figures.R](./DMR.figures.R): `DMR_figures` R function for DMR analysis and visualization.
 * [DMR.intersect.sh](./DMR.intersect.sh): intersecting DMRs with genomic regions and report CpG% breakdown in intergenic, intron, exon, gene, promoter regions.     
