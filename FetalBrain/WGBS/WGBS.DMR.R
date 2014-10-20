acd87993dc8d0a1c6617dcc89a219d3f5547cf18
Gloria Li <glorialuolanli@gmail.com>
2014-10-20 15:12:31 -0700

    Update WGBS and MeDIP-Cortex vs GE summaries.

--- FetalBrain/WGBS/WGBS.DMR.R
+++ FetalBrain/WGBS/WGBS.DMR.R
@@ -31,21 +31,21 @@ dev.off()
 #' preferable parameters: p = 0.005 (two-sided), delta = 0.5, m = 0.75, size = 300
 setwd("~/快盘/FetalBrain/WGBS/DMR/")
 DMR_WGBS_summary <- select(filter(DMR_test_summary, size==300, p=="p=0.005", delta=="delta=0.5"), Sample, contains("DMR"))
-Cortex02_GE02_DMR <- read.delim("DMR.Cortex-HuFNSC02_GE-HuFNSC02.m0.75.p0.005.d0.5.s300.c3", as.is = T, head = F)
-Cortex02_GE02_DMR_figures <- DMR_figures(Cortex02_GE02_DMR, sample1 = "Cortex-HuFNSC02", sample2 = "GE-HuFNSC02")
-(Cortex02_GE02_DMR_figures$length)
-(Cortex02_GE02_DMR_figures$count)
-(Cortex02_GE02_DMR_figures$dis)
-(Cortex02_GE02_DMR_figures$freq)
-(Cortex02_GE02_DMR_figures$pos + geom_point(aes(x = (as.numeric(chr) + 0.25*DM), y = pos, color = factor(DM, levels = c("1", "-1"))), position = position_jitter(width = 0.05), size = 1.5, alpha = 0.5))
-Cortex04_GE04_DMR <- read.delim("DMR.Cortex-HuFNSC04_GE-HuFNSC04.m0.75.p0.005.d0.5.s300.c3", as.is = T, head = F)
-Cortex04_GE04_DMR_figures <- DMR_figures(Cortex04_GE04_DMR, sample1 = "Cortex-HuFNSC04", sample2 = "GE-HuFNSC04")
-(Cortex04_GE04_DMR_figures$length)
-(Cortex04_GE04_DMR_figures$count)
-(Cortex04_GE04_DMR_figures$dis)
-(Cortex04_GE04_DMR_figures$freq)
-(Cortex04_GE04_DMR_figures$pos + geom_point(aes(x = (as.numeric(chr) + 0.25*DM), y = pos, color = factor(DM, levels = c("1", "-1"))), position = position_jitter(width = 0.05), size = 1.5, alpha = 0.5))
-DMR_freq_neurospheres <- rbind(cbind(Cortex02_GE02_DMR_figures$freq$data, donor = "HuFNSC02"), cbind(Cortex04_GE04_DMR_figures$freq$data, donor = "HuFNSC04"))
+Cortex02_GE02_DMR_WGBS <- read.delim("DMR.Cortex-HuFNSC02_GE-HuFNSC02.m0.75.p0.005.d0.5.s300.c3", as.is = T, head = F)
+Cortex02_GE02_DMR_WGBS_figures <- DMR_figures(Cortex02_GE02_DMR_WGBS, sample1 = "Cortex-HuFNSC02", sample2 = "GE-HuFNSC02")
+(Cortex02_GE02_DMR_WGBS_figures$length)
+(Cortex02_GE02_DMR_WGBS_figures$count)
+(Cortex02_GE02_DMR_WGBS_figures$dis)
+(Cortex02_GE02_DMR_WGBS_figures$freq)
+(Cortex02_GE02_DMR_WGBS_figures$pos + geom_point(aes(x = (as.numeric(chr) + 0.25*DM), y = pos, color = factor(DM, levels = c("1", "-1"))), position = position_jitter(width = 0.05), size = 1.5, alpha = 0.5))
+Cortex04_GE04_DMR_WGBS <- read.delim("DMR.Cortex-HuFNSC04_GE-HuFNSC04.m0.75.p0.005.d0.5.s300.c3", as.is = T, head = F)
+Cortex04_GE04_DMR_WGBS_figures <- DMR_figures(Cortex04_GE04_DMR_WGBS, sample1 = "Cortex-HuFNSC04", sample2 = "GE-HuFNSC04")
+(Cortex04_GE04_DMR_WGBS_figures$length)
+(Cortex04_GE04_DMR_WGBS_figures$count)
+(Cortex04_GE04_DMR_WGBS_figures$dis)
+(Cortex04_GE04_DMR_WGBS_figures$freq)
+(Cortex04_GE04_DMR_WGBS_figures$pos + geom_point(aes(x = (as.numeric(chr) + 0.25*DM), y = pos, color = factor(DM, levels = c("1", "-1"))), position = position_jitter(width = 0.05), size = 1.5, alpha = 0.5))
+DMR_freq_neurospheres <- rbind(cbind(Cortex02_GE02_DMR_WGBS_figures$freq$data, donor = "HuFNSC02"), cbind(Cortex04_GE04_DMR_WGBS_figures$freq$data, donor = "HuFNSC04"))
 DMR_freq_neurospheres_figure <- ggplot(DMR_freq_neurospheres, aes(x = chr, y = freq, fill = DM)) + 
   geom_bar(position = "identity", stat = "identity", width = 0.8) + 
   facet_wrap(~ donor) + 
@@ -59,11 +59,11 @@ ggsave(DMR_freq_neurospheres_figure, file = "DMRfreq_neurospheres.pdf", width = 
 
 
 #` GREAT enrichment analysis
-(GREAT_Cortex02.UMR <- enrich_GREAT(file = "DMR.Cortex-HuFNSC02_GE-HuFNSC02.hypo", name = "HuFNSC02-Cortex.UMRs", height = 12))
-(GREAT_GE02.UMR <- enrich_GREAT(file = "DMR.Cortex-HuFNSC02_GE-HuFNSC02.hyper", name = "HuFNSC02-GE.UMRs"))
-(GREAT_Cortex04.UMR <- enrich_GREAT(file = "DMR.Cortex-HuFNSC04_GE-HuFNSC04.hypo", name = "HuFNSC04-Cortex.UMRs", height = 13))
-(GREAT_GE04.UMR <- enrich_GREAT(file = "DMR.Cortex-HuFNSC04_GE-HuFNSC04.hyper", name = "HuFNSC04-GE.UMRs", height = 3))
-GOBP_Cortex02.UMR <- cbind(GREAT_Cortex02.UMR$data, UMR = "Cortex UMRs")
+(GREAT_Cortex02.UMR_WGBS <- enrich_GREAT(file = "DMR.Cortex-HuFNSC02_GE-HuFNSC02.hypo", name = "HuFNSC02-Cortex.UMRs", height = 12))
+(GREAT_GE02.UMR_WGBS <- enrich_GREAT(file = "DMR.Cortex-HuFNSC02_GE-HuFNSC02.hyper", name = "HuFNSC02-GE.UMRs"))
+(GREAT_Cortex04.UMR_WGBS <- enrich_GREAT(file = "DMR.Cortex-HuFNSC04_GE-HuFNSC04.hypo", name = "HuFNSC04-Cortex.UMRs", height = 13))
+(GREAT_GE04.UMR_WGBS <- enrich_GREAT(file = "DMR.Cortex-HuFNSC04_GE-HuFNSC04.hyper", name = "HuFNSC04-GE.UMRs", height = 3))
+GOBP_Cortex02.UMR <- cbind(GREAT_Cortex02.UMR_WGBS$data, UMR = "Cortex UMRs")
 GOBP_Cortex02.UMR <- GOBP_Cortex02.UMR[as.character(GOBP_Cortex02.UMR$Category) == "GOBP", ]
 GOBP_Cortex02.UMR_plot <- ggplot(data = GOBP_Cortex02.UMR, aes(Term, -log10(FDR))) +
   geom_bar(fill = "blue", stat = "identity", width = .5) + 
@@ -75,7 +75,7 @@ GOBP_Cortex02.UMR_plot <- ggplot(data = GOBP_Cortex02.UMR, aes(Term, -log10(FDR)
   scale_fill_hue(l = 40)
 (GOBP_Cortex02.UMR_plot + ggtitle("GOBP GREAT enrichment for Cortex UMRs"))
 ggsave(GOBP_Cortex02.UMR_plot, file = "./Enrich/GOBP_HuFNSC02_Cortex.UMR_enrich.pdf", height = 3, width = 6)
-GOBP_GE02.UMR <- cbind(GREAT_GE02.UMR$data, UMR = "GE UMRs")
+GOBP_GE02.UMR <- cbind(GREAT_GE02.UMR_WGBS$data, UMR = "GE UMRs")
 GOBP_GE02.UMR <- GOBP_GE02.UMR[as.character(GOBP_GE02.UMR$Category) == "GOBP", ]
 GOBP_GE02.UMR_plot <- ggplot(data = GOBP_GE02.UMR, aes(Term, -log10(FDR))) +
   geom_bar(fill = "blue", stat = "identity", width = .5) + 
@@ -207,8 +207,8 @@ write.table(Cortex04_UMR_pcPromoter_DE, file = "DMR_pcPromoter_DE.Cortex-HuFNSC0
 # (Cortex04_UMR_pcPromoter_DE_DAVID <- enrich(name = "DMR_pcPromoter_DE.Cortex_UMRs-HuFNSC04", erminej = F))
 
 #' Venn diagrams 
-venn_Cortex_UMR_WGBS <- draw.pairwise.venn(area1 = nrow(filter(Cortex02_GE02_DMR, V5 == -1)), area2 = nrow(filter(Cortex04_GE04_DMR, V5 == -1)), cross.area = 179, category = c("HuFNSC02", "HuFNSC04"), fill = c("red", "blue"))
-venn_GE_UMR_WGBS <- draw.pairwise.venn(area1 = nrow(filter(Cortex02_GE02_DMR, V5 == 1)), area2 = nrow(filter(Cortex04_GE04_DMR, V5 == 1)), cross.area = 10, category = c("HuFNSC02", "HuFNSC04"), fill = c("red", "blue"))
+venn_Cortex_UMR_WGBS <- draw.pairwise.venn(area1 = nrow(filter(Cortex02_GE02_DMR_WGBS, V5 == -1)), area2 = nrow(filter(Cortex04_GE04_DMR_WGBS, V5 == -1)), cross.area = 179, category = c("HuFNSC02", "HuFNSC04"), fill = c("red", "blue"))
+venn_GE_UMR_WGBS <- draw.pairwise.venn(area1 = nrow(filter(Cortex02_GE02_DMR_WGBS, V5 == 1)), area2 = nrow(filter(Cortex04_GE04_DMR_WGBS, V5 == 1)), cross.area = 10, category = c("HuFNSC02", "HuFNSC04"), fill = c("red", "blue"))
 Cortex_UMR_pcGene_WGBS <- list(HuFNSC02 = Cortex02_UMR_pcGene$V5, HuFNSC04 = Cortex04_UMR_pcGene$V5)
 venn_Cortex_UMR_pcGene_WGBS <- venn.diagram(Cortex_UMR_pcGene_WGBS, filename = NULL, fill = c("red", "blue"), main = "Venn diagram of genes with Cortex UMRs")
 GE_UMR_pcGene_WGBS <- list(HuFNSC02 = GE02_UMR_pcGene$V5, HuFNSC04 = GE04_UMR_pcGene$V5)
@@ -290,22 +290,22 @@ DMR_TF_tall$Asymmetry <- NA
 DMR_TF_tall[DMR_TF_tall$Ratio > 1,]$Asymmetry <- "Cortex enriched"
 DMR_TF_tall[DMR_TF_tall$Ratio < 1,]$Asymmetry <- "GE enriched"
 DMR_TF_tall[DMR_TF_tall$Ratio == 1,]$Asymmetry <- "Equal"
-(DMR_TF_aymmetry <- ggplot(DMR_TF_tall, aes(x = Cortex_UMR, y = GE_UMR, label = TF, color = Asymmetry)) + 
+(DMR_TF_figure <- ggplot(DMR_TF_tall, aes(x = Cortex_UMR, y = GE_UMR, label = TF, color = Asymmetry)) + 
    geom_point() + 
    geom_abline(intercept=0, slope=1) + 
-   geom_text(data = subset(DMR_TF_tall, (Donor == "HuFNSC02" & (Ratio > 3 | Ratio <= 0.5)) | (Donor == "HuFNSC04" & (Ratio > 15 | Ratio <= 0.5))), angle = 45, size = 4, hjust = 0.2, vjust = 0.2) + 
+   geom_text(data = subset(DMR_TF_tall, (Donor == "HuFNSC02" & (Ratio > 3 | Ratio <= 1/3)) | (Donor == "HuFNSC04" & (Ratio > 15 | Ratio <= 0.5))), angle = 45, size = 4, hjust = 0.2, vjust = 0.2) + 
    facet_wrap(~ Donor) + 
    scale_x_log10() + 
    scale_y_log10() + 
    scale_color_hue(l = 50) + 
    theme_bw())
-ggsave(DMR_TF_aymmetry, file = "DMR_TF_aymmetry.pdf", height = 6)
+ggsave(DMR_TF_figure, file = "DMR_TF_WGBS.pdf", height = 6)
 
-save(DM_test_summary, DMR_test_summary, DMR_WGBS_summary, Cortex02_GE02_DMR, Cortex02_GE02_DMR_figures, Cortex04_GE04_DMR, Cortex04_GE04_DMR_figures, 
-     GREAT_Cortex02.UMR, GREAT_GE02.UMR, GREAT_Cortex04.UMR, GREAT_GE04.UMR, valid_boxplot, genomicBreak_WGBS_figure, DMR_WGBS_gene_summary,
+save(DM_test_summary, DMR_test_summary, DMR_WGBS_summary, Cortex02_GE02_DMR_WGBS, Cortex02_GE02_DMR_WGBS_figures, Cortex04_GE04_DMR_WGBS, Cortex04_GE04_DMR_WGBS_figures, 
+     GREAT_Cortex02.UMR_WGBS, GREAT_GE02.UMR_WGBS, GREAT_Cortex04.UMR_WGBS, GREAT_GE04.UMR_WGBS, valid_boxplot, genomicBreak_WGBS_figure, DMR_WGBS_gene_summary,
      venn_Cortex_UMR_WGBS, venn_Cortex_UMR_pcGene_WGBS, venn_Cortex_UMR_pcPromoter_WGBS, venn_Cortex_UMR_pcPromoter_DE_WGBS, 
      venn_GE_UMR_WGBS, venn_GE_UMR_pcGene_WGBS, venn_GE_UMR_pcPromoter_WGBS, venn_GE_UMR_pcPromoter_DE_WGBS, 
      Cortex02_UMR_pcPromoter, Cortex02_UMR_pcPromoter_DE, GE02_UMR_pcPromoter, GE02_UMR_pcPromoter_DE, 
      Cortex04_UMR_pcPromoter, Cortex04_UMR_pcPromoter_DE, GE04_UMR_pcPromoter, GE04_UMR_pcPromoter_DE, 
-     DMR_TF, DMR_TF_aymmetry, 
+     DMR_TF, DMR_TF_figure, 
      file = "~/快盘/FetalBrain/WGBS/WGBS.DMR.Rdata")
