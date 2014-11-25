72ed94f3b8ce9382e07b2a03f576b6f2c006e6b5
Gloria Li <glorialuolanli@gmail.com>
2014-11-25 09:58:42 -0800

    Update FetalBrain QC summary metrics.

--- FetalBrain/bamstats.sh
+++ FetalBrain/bamstats.sh
@@ -46,17 +46,18 @@ less /projects/epigenomics/users/lli/FetalBrain/FetalBrainID_other.txt | awk '{c
 # combine reports together
 dir=/projects/epigenomics/users/acarles/qc
 cd $dir
-cp /projects/epigenomics/users/acarles/qc/summaryTemplate.11 summary.xls
-for file in report_*.20141124; do
-less summary.xls | sort -k1,1 > x;
+cp /projects/epigenomics/users/acarles/qc/summaryTemplate.11 FetalBrain_QCsummary.xls
+for file in report_*.20141124 report_*.20141125; do
+less FetalBrain_QCsummary.xls | sort -k1,1 > x;
 echo "$file"
 rfile=$dir/$file
 c=$(less $rfile | wc -l);
 if [ "$c" -eq "11" ]; then
 less $rfile | awk '{c=c+1; print c"\t"$3}' | sort -k1,1 > y;
-join x y | sed 's/ /\t/g' | sort -k1,1n > summary.xls ;
+join x y | sed 's/ /\t/g' | sort -k1,1n > FetalBrain_QCsummary.xls ;
 rm -rf x y;
 else
   echo "$file : report has a wrong format. Skipping."
 fi
 done
+rm report_*.20141124 report_*.20141125
