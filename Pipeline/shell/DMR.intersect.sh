3c5fa5e0ad9244effe3c431b52d31d924974d909
Gloria Li <glorialuolanli@gmail.com>
2014-11-27 15:12:55 -0800

    Update DMR intersect pipeline script to report fold enrichment directly.

--- Pipeline/shell/DMR.intersect.sh
+++ Pipeline/shell/DMR.intersect.sh
@@ -2,7 +2,7 @@
 
 # intersect with genomic regions
 if [ "$1" == "-h" ] ; then
-    echo -e "Intersecting DM CpGs with genomic regions
+    echo -e "Intersecting DM CpGs with genomic regions and report fold enrichment for each genomic feature 
 Usage: `basename $0` -d <dirIn>
     <dirIn>: input directory with DMR bed files"
     exit 0
@@ -17,7 +17,6 @@ do
 done
 dirOut=$dirIn/CpG
 mkdir -p $dirOut
-> $dirOut/genomic.breakdown.summary
 cd $dirIn
 echo -e "Output files:
 $dirOut/DMR.<name>.CpG.bed: chr\tstart\tend\tDMR_ID
@@ -27,7 +26,14 @@ $dirOut/DMR.<name>.CpG_promoter.bed: chr\tstart\tend\tDMR_ID\tEnsembl
 $dirOut/DMR.<name>.CpG_gene_pc.bed: chr\tstart\tend\tDMR_ID\tEnsembl
 $dirOut/DMR.<name>.CpG_promoter_pc.bed: chr\tstart\tend\tDMR_ID\tEnsembl
 $dirOut/DMR.<name>.CpG_CGI.bed: chr\tstart\tend\tDMR_ID\tCGI_ID
-$dirOut/genomic.breakdown.summary: Name\tTotal No. of DM CpGs\tIntergenic\tIntron\tExon\tGene\tPromoter\tCGI\n"
+$dirOut/genomic.breakdown.summary: Name\tTotal No. of CpGs\tIntergenic\tIntron\tExon\tGene\tPromoter\tCGI\n"
+all_total=28217448 # wc -l /home/lli/hg19/CG.BED
+all_gene=18270435 # /gsc/software/linux-x86_64-centos5/bedtools-2.17.0/bin/intersectBed -a /home/lli/hg19/CG.BED -b /home/lli/hg19/hg19v65_genes.bed -wa -wb | wc -l
+all_exon=7064050 # /gsc/software/linux-x86_64-centos5/bedtools-2.17.0/bin/intersectBed -a /home/lli/hg19/CG.BED -b /home/lli/hg19/hg19v65_exons.bed -wa -wb | wc -l
+all_promoter=3727169 # /gsc/software/linux-x86_64-centos5/bedtools-2.17.0/bin/intersectBed -a /home/lli/hg19/CG.BED -b /home/lli/hg19/hg19v65_genes_TSS_1500.bed -wa -wb | wc -l
+all_CGI=2089538 # /gsc/software/linux-x86_64-centos5/bedtools-2.17.0/bin/intersectBed -a /home/lli/hg19/CG.BED -b /projects/epigenomics/resources/UCSC_hg19/CGI/CGI.forProfiles.BED -wa -wb | wc -l
+> $dirOut/genomic.breakdown.summary
+echo -e "$all_total\t$all_gene\t$all_exon\t$all_promoter\t$all_CGI" | awk '{print "ExpectedPercent\t"$1"\t"($1-$2)/$1"\t"($2-$3)/$1"\t"$3/$1"\t"$2/$1"\t"$4/$1"\t"$5/$1}' >> $dirOut/genomic.breakdown.summary
 for dmr in DMR.*.bed
 do
     name=$(echo $dmr | sed -e s/'DMR.'//g)
@@ -45,6 +51,6 @@ do
     exon=`less $dirOut/DMR.$name.CpG_exon.bed | awk '{if(!($4 in h)){s=s+1; h[$4]=1}} END{print s}'`
     promoter=`less $dirOut/DMR.$name.CpG_promoter.bed | awk '{if(!($4 in h)){s=s+1; h[$4]=1}} END{print s}'`
     CGI=`wc -l $dirOut/DMR.$name.CpG_CGI.bed | cut -d' ' -f 1`
-    echo -e "$name\t$total\t$gene\t$exon\t$promoter\t$CGI" | awk '{print $1"\t"$2"\t"($2-$3)/$2"\t"($3-$4)/$2"\t"$4/$2"\t"$3/$2"\t"$5/$2"\t"$6/$2}' >> $dirOut/genomic.breakdown.summary
+    echo -e "$name\t$total\t$gene\t$exon\t$promoter\t$CGI\t$all_total\t$all_gene\t$all_exon\t$all_promoter\t$all_CGI" | awk '{print $1"\t"$2"\t"(($2-$3)/$2)/(($7-$8)/$7)"\t"(($3-$4)/$2)/(($8-$9)/$7)"\t"($4/$2)/($9/$7)"\t"($3/$2)/($8/$7)"\t"($5/$2)/($10/$7)"\t"($6/$2)/($11/$7)}' >> $dirOut/genomic.breakdown.summary
 done
 
