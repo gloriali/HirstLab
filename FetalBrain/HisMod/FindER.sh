e0d07a42dd241ce1235668a161707cb4648ada44
Gloria Li <glorialuolanli@gmail.com>
2015-01-25 16:38:41 -0800

    Switch to stable FindER and use PET mappability files.

--- FetalBrain/HisMod/FindER.sh
+++ FetalBrain/HisMod/FindER.sh
@@ -5,7 +5,7 @@ dirIn=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/bam/
 cd $dirIn
 for file in *.bam
 do
-    echo "Processing" $file
+    echo "Indexing" $file
     /gsc/software/linux-x86_64-centos5/samtools-0.1.18/bin/samtools index $file
 done
 
@@ -22,7 +22,7 @@ do
     for minER in 250 500 1000
     do
         echo "Processing FindER on" $bam $minER
-        /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/ew/Solexa_Java/jarsToDeploy/FindER2.jar -i $bam -r $reg -o $dirOut -v -m $map -minER $minER -info -cs > $dirOut/FindER_scan.$bam.log
+        /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/bin/Solexa_Java/FindER.jar -i $bam -r $reg -o $dirOut -v -m $map -minER $minER -info -cs > $dirOut/FindER_scan.$bam.log
     done
 done
 ### 1000bp minER supported by 500 and 250bp
@@ -42,7 +42,7 @@ rm -rf x y
 # FindER for H3K4me3
 ## with three sizes of minimal enriched regions 200, 300bp
 reg=/projects/epigenomics/nci_rt/ChIPseq_RT/FindER/chr.regions
-map=/projects/mbilenky/FindER/synthetic/hg19/smap/
+map=/projects/mbilenky/FindER/synthetic/hg19/map/
 dirIn=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/bam/
 dirOut=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/FindER/H3K4me3/
 mkdir -p $dirOut
@@ -52,14 +52,14 @@ do
     for minER in 200 300 
     do
         echo "Processing FindER on" $bam $minER
-        /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/ew/Solexa_Java/jarsToDeploy/FindER2.jar -i $bam -r $reg -o $dirOut -v -m $map -minER $minER -info -cs > $dirOut/FindER_scan.$bam.log
+        /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/bin/Solexa_Java/FindER.jar -i $bam -r $reg -o $dirOut -v -m $map -minER $minER -info -cs > $dirOut/FindER_scan.$bam.log
     done
 done
 
 # FindER for H3K9me3
 ## with three sizes of minimal enriched regions 250, 500, 1000bp
 reg=/projects/epigenomics/nci_rt/ChIPseq_RT/FindER/chr.regions
-map=/projects/mbilenky/FindER/synthetic/hg19/smap/
+map=/projects/mbilenky/FindER/synthetic/hg19/map/
 dirIn=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/bam/
 dirOut=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/FindER/H3K9me3/
 mkdir -p $dirOut
@@ -69,7 +69,7 @@ do
     for minER in 250 500 1000
     do
         echo "Processing FindER on" $bam $minER
-        /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/ew/Solexa_Java/jarsToDeploy/FindER2.jar -i $bam -r $reg -o $dirOut -v -m $map -minER $minER -info -cs > $dirOut/FindER_scan.$bam.log
+        /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/bin/Solexa_Java/FindER.jar -i $bam -r $reg -o $dirOut -v -m $map -minER $minER -info -cs > $dirOut/FindER_scan.$bam.log
     done
 done
 ### 1000bp minER supported by 500 and 250bp
@@ -89,7 +89,7 @@ rm -rf x y
 # FindER for H3K27me3
 ## with three sizes of minimal enriched regions 250, 500, 1000bp
 reg=/projects/epigenomics/nci_rt/ChIPseq_RT/FindER/chr.regions
-map=/projects/mbilenky/FindER/synthetic/hg19/smap/
+map=/projects/mbilenky/FindER/synthetic/hg19/map/
 dirIn=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/bam/
 dirOut=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/FindER/H3K27me3/
 mkdir -p $dirOut
@@ -99,7 +99,7 @@ do
     for minER in 250 500 1000
     do
         echo "Processing FindER on" $bam $minER
-        /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/ew/Solexa_Java/jarsToDeploy/FindER2.jar -i $bam -r $reg -o $dirOut -v -m $map -minER $minER -info -cs > $dirOut/FindER_scan.$bam.log
+        /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/bin/Solexa_Java/FindER.jar -i $bam -r $reg -o $dirOut -v -m $map -minER $minER -info -cs > $dirOut/FindER_scan.$bam.log
     done
 done
 ### 1000bp minER supported by 500 and 250bp
@@ -119,7 +119,7 @@ rm -rf x y
 # FindER for H3K36me3
 ## with three sizes of minimal enriched regions 250, 500, 1000bp
 reg=/projects/epigenomics/nci_rt/ChIPseq_RT/FindER/chr.regions
-map=/projects/mbilenky/FindER/synthetic/hg19/smap/
+map=/projects/mbilenky/FindER/synthetic/hg19/map/
 dirIn=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/bam/
 dirOut=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/FindER/H3K36me3/
 mkdir -p $dirOut
@@ -129,7 +129,7 @@ do
     for minER in 250 500 1000
     do
         echo "Processing FindER on" $bam $minER
-        /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/ew/Solexa_Java/jarsToDeploy/FindER2.jar -i $bam -r $reg -o $dirOut -v -m $map -minER $minER -info -cs > $dirOut/FindER_scan.$bam.log
+        /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/bin/Solexa_Java/FindER.jar -i $bam -r $reg -o $dirOut -v -m $map -minER $minER -info -cs > $dirOut/FindER_scan.$bam.log
     done
 done
 ### 1000bp minER supported by 500 and 250bp
@@ -148,7 +148,7 @@ rm -rf x y
 
 # FindER for Input
 reg=/projects/epigenomics/nci_rt/ChIPseq_RT/FindER/chr.regions
-map=/projects/mbilenky/FindER/synthetic/hg19/smap/
+map=/projects/mbilenky/FindER/synthetic/hg19/map/
 dirIn=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/bam/
 dirOut=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/FindER/Input/
 mkdir -p $dirOut
@@ -156,6 +156,6 @@ cd $dirIn
 for bam in *.Input.*.bam
 do
     echo "Processing FindER on" $bam
-    /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/ew/Solexa_Java/jarsToDeploy/FindER2.jar -i $bam -r $reg -o $dirOut -v -m $map -bin 10 -info -cs > $dirOut/FindER_scan.$bam.log
+    /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx10G /home/mbilenky/bin/Solexa_Java/FindER.jar -i $bam -r $reg -o $dirOut -v -m $map -bin 10 -info -cs > $dirOut/FindER_scan.$bam.log
 done
 
