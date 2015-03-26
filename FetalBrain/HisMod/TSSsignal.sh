82fba7094f0de335ebf9fc080520383fa2ac33ae
Gloria Li <glorialuolanli@gmail.com>
2015-03-25 18:07:03 -0700

    Update HisMod TSS/genebody signal.

--- FetalBrain/HisMod/TSSsignal.sh
+++ FetalBrain/HisMod/TSSsignal.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-cd /home/lli/FetalBrain/HisMod/signal/
 chr=/home/mbilenky/UCSC_chr/hg19_auto_XY.chrom.sizes
 reg=/projects/epigenomics/resources/Ensembl/hg19v65/hg19v65_genes_TSS_2000
-out=/home/lli/FetalBrain/HisMod/signal/
+out=/projects/epigenomics/users/lli/FetalBrain/ChIPseq/signal/
+mkdir -p $out
+cd /home/lli/FetalBrain/HisMod/wigs/
 
-for wig in /home/lli/FetalBrain/HisMod/wigs/*.wig.gz
+for wig in *.wig.gz
 do
-    name=$(basename $wig | sed 's/.gz//' | sed 's/.wig//')
+    name=$(echo $wig | cut -d'.' -f1)
     echo "Processing $name"
     /gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java -jar -Xmx15G /home/mbilenky/bin/Solexa_Java/RegionsCoverageFromWigCalculator.jar -w $wig -r $reg -o $out -s $chr -n $name > $out$name.log
 done
