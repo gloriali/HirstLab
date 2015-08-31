eb7bd0402515bfb21ff196ca7ab03174bacd8fe9
Luolan (Gloria) Li <gloriali@users.noreply.github.com>
2015-08-31 13:47:06 -0700

    Update file format.

--- Pipeline/shell/WGBS.combine.sh
+++ Pipeline/shell/WGBS.combine.sh
@@ -26,7 +26,7 @@ done
 mkdir -p $dirOut
 
 echo "Combining strands for "$file" output to "$dirOut
-less $dirIn/$file | awk '{print "chr"$1":"$2"\t"$5"\t"$6}' > $dirOut/$file.tmp
+less $dirIn/$file | awk '{if($1~/chr/){print $1":"$2"\t"$5"\t"$6} else{print "chr"$1":"$2"\t"$5"\t"$6}}' > $dirOut/$file.tmp
 awk 'NR==FNR {h[$1]=$2; next} {if($1 in h){print $0"\t"h[$1]}}' $strand $dirOut/$file.tmp > $dirOut/$file.tmp.join
 less $dirOut/$file.tmp.join | awk '{c[$4]=c[$4]+$2; t[$4]=t[$4]+$3; count[$4]=count[$4]+1} END{for(i in count){if(count[i]>2){print i"\t"c[i]"\t"t[i]"\t"count[i] >> "'$dirOut'""/ERROR.""'$file'"".error"}}; for(i in c){m=c[i]/(c[i]+t[i]); print i"\t"c[i]"\t"t[i]"\t"m}}' > $dirOut/$file.combine
 rm $dirOut/$file.tmp $dirOut/$file.tmp.join
