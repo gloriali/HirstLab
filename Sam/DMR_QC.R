a9695024c4810acb0ba2305ed1ada88f79f60f30
Sam McConnell <sammcconnell@live.ca>
2016-03-07 18:19:21 -0800

    Added new function to make boxplots from list.

--- Sam/DMR_QC.R
+++ Sam/DMR_QC.R
@@ -1,5 +1,6 @@
+library(ggplot2)
 
-# produce uniquely named data.frames for each input
+# bed file of DMRs -> data frame containing chr's and their average DMR lengths
 
 DMR_averages <- function(dmr) {
   dmrl = list()
@@ -13,6 +14,8 @@ DMR_averages <- function(dmr) {
 }
 
 # Take input directory and filename pattern, produce dataframes of all that match.
+# Assumes that the files will contain a DMR.[...]-[...].methyl pattern
+# Be careful when entering patterns that you're only including files that you want
 
 list.DMR <- function(pattern, path = "") {
   
@@ -23,13 +26,26 @@ list.DMR <- function(pattern, path = "") {
     filename = sub("DMR\\.(.*)-(.*)\\.methyl.*", "\\1_\\2", i)
     assign(filename, read.delim(paste0(getwd(), "/", path, i), header = F))
     tables[[filename]] = get(filename)
+    
+    # Assumes chromosomes are misordered from bash (eg. chr1, chr10, chr11, ..., chr19, chr2, chr 20, etc.) and reorders them
+    tables[[filename]]$V1 = factor(tables[[filename]]$V1, levels = levels(factor(tables[[filename]]$V1))[c(1, 12, 16:22, 2:11, 13:15, 23, 24)])
   }
   tables
 }
 
 # Take input directory and filename pattern, produce dataframes of chr's and average DMR lengths
+# Assumes that the files will contain a DMR.[...]-[...].methyl pattern
+# Be careful when entering patterns that you're only including files that you want
 
 DMR.lengths <- function(pattern, path = "") {
   lapply(list.DMR(pattern, path), DMR_averages)
 }
+
+# Given a list of BED formatted DMRs, y: create a boxplot of average DMR length for each chromosome for every element of y
+# ~~~~ Make axis labels next ~~~~~
+
+DMR.len.plot <- function(y) {
+  lapply(y, function(i) ggplot(data = i, aes(x = V1, y = V7, fill = V1)) + geom_boxplot() + coord_cartesian(ylim = c(0,1500)))
+}
+
 
