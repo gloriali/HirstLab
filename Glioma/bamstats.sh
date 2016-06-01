7aa53561842fba586c4bfb7897591a8b0b26934c
Gloria Li <glorialuolanli@gmail.com>
2016-06-01 12:07:27 -0700

    Add library details for glioma samples.

--- Glioma/bamstats.sh
+++ Glioma/bamstats.sh
@@ -11,4 +11,6 @@ less $dirOut/bamstas2report.log | grep 'ERROR' > $dirOut/bamstas2report.error
 # generate bamstats files for libraries without
 less $dirOut/bamstas2report.error | awk '{print $6; gsub("bamstats", "bam"); cmd="/gsc/QA-bio/sbs-solexa/opt/linux-x86_64/bwa_stats_0.1.3/bamStats.py -g 2864785220 -q 10 -b " $7 " > ""'$dirOut'" $6 ".bamstats"; system(cmd); cmd2="/home/lli/HirstLab/Pipeline/shell/bamstats2report.sh ""'$dirOut'"" "$6" ""'$dirOut'" $6 ".bamstats"; system(cmd2)}'
 /home/lli/HirstLab/Pipeline/shell/bamstats2report.combine.sh $dirOut $dirOut
+# add metadata info
+awk 'NR==FNR {donor[$3]=$1; assay[$3]=$2; next} {print $0"\t"donor[$1]"\t"assay[$1]}' /projects/epigenomics2/users/lli/glioma/glioma_libraries.tsv $dirOut/summary.txt > $dirOut/summary.tsv
 
