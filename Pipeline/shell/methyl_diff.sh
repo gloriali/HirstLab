00c4ba55ca226c09d86e33040285fddad3549718
Luolan (Gloria) Li <gloriali@users.noreply.github.com>
2016-06-13 14:53:07 -0700

    small edits.

--- Pipeline/shell/methyl_diff.sh
+++ Pipeline/shell/methyl_diff.sh
@@ -5,8 +5,8 @@ if [ "$1" == "-h" ] ; then
 Usage: `basename $0` -i <dirIn> -o <dirOut> -f1 <file1> -f2 <file2> -n <name> -p <p-value> -d <delta> -m <methylation> -c <coverage>
     <dirIn>: input directory
     <dirOut>: ourput directory
-    <file1>: input for sample1, format: chr\tposition\tstrand\tconverted\tunconverted (novoalign output .5mC.CpG file)
-    <file2>: input for sample2, format: chr\tposition\tstrand\tconverted\tunconverted (novoalign output .5mC.CpG file)
+    <file1>: input for sample1, format: chr\tposition\tstrand\tconverted\tunconverted (novoalign output .5mC.CpG file) or chr\tstart\tend\tconverted\tunconverted\tfractional (WGBS.combine.sh output)   
+    <file2>: input for sample2, format: chr\tposition\tstrand\tconverted\tunconverted (novoalign output .5mC.CpG file) or chr\tstart\tend\tconverted\tunconverted\tfractional (WGBS.combine.sh output)   
     <name>: output file name
     <p-value>: p-value cutoff for methyl_diff, default to 0.0005
     <delta>: min difference in fractional methylation, default to 0.6
