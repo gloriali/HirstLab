38ca226c89b596b3193d7bd24b6de5d705cf6562
Gloria Li <glorialuolanli@gmail.com>
2016-06-17 10:40:56 -0700

    Add output R figure.

--- Pipeline/shell/ER.unique.sh
+++ Pipeline/shell/ER.unique.sh
@@ -29,6 +29,7 @@ echo "Test whether $region are enriched in $wig, output to $dirOut/$name"
 JAVA=/gsc/software/linux-x86_64-centos5/java-1.7.0-u13/bin/java
 RegCov=/home/mbilenky/bin/Solexa_Java/RegionsCoverageFromWigCalculator.jar
 BEDTOOLS='/gsc/software/linux-x86_64-centos5/bedtools/bedtools-2.25.0/bin/'
+R=/gsc/software/linux-x86_64-centos5/R-3.1.1/bin/R
 chr=/home/mbilenky/UCSC_chr/hg19_auto_XY.chrom.sizes
 
 ## calculate region signal from wig
@@ -41,11 +42,21 @@ $BEDTOOLS/shuffleBed -i $region -g $chr -excl $excl | awk '{print $1"\t"$2"\t"$3
 ## calculate coverage for background regions
 $JAVA -jar -Xmx15G $RegCov -w $wig -r $dirOut/$name.background.bed -o $dirOut -c $chr -n $name.background >> $dirOut/$name.log
 
-## choosing cutoff: 90% quantile of coverage for background regions
+## plot and cutoff: 90% quantile of coverage for background regions
+echo "signal <- read.delim(\"$(ls $dirOut/*.$name.signal.coverage)\", as.is = T, head = F, col.names = c(\"chr\", \"start\", \"end\", \"ID\", \"cov\", \"max\"))
+background <- read.delim(\"$(ls $dirOut/*.$name.background.coverage)\", as.is = T, head = F, col.names = c(\"chr\", \"start\", \"end\", \"ID\", \"cov\", \"max\"))
+pdf(\"$dirOut/$name.pdf\")
+plot(c(0, 20), c(0, 1), type = \"n\", main = \"$name\", xlab = \"average coverage\", ylab = \"ecdf\")
+lines(ecdf(signal\$cov), col = \"red\")
+lines(ecdf(background\$cov), col = \"blue\")
+abline(v = quantile(background\$cov, 0.9), col = \"blue\")
+dev.off()
+" > $dirOut/$name.R
+$R CMD BATCH $dirOut/$name.R
 cutoff=$(less $dirOut/*.$name.background.coverage | awk '{print $5}' | sort -k1,1n | awk '{c[NR]=$1} END{print c[NR-int(NR/10)]}')
 echo "Coverage cutoff: $cutoff"
 
 ## output
 less $dirOut/*.$name.signal.coverage | awk '{if($5 <= '"$cutoff"'){print $0}}' > $dirOut/$name.unique
-rm $dirOut/*$name.signal* $dirOut/*$name.background*
+rm $dirOut/*$name.signal* $dirOut/*$name.background* $dirOut/$name.R
 
