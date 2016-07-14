f31b4baa7b43223993a55283c457d55695508083
Luolan (Gloria) Li <gloriali@users.noreply.github.com>
2016-07-14 12:16:53 -0700

    debug.

--- Pipeline/shell/bamstats2report.combine.sh
+++ Pipeline/shell/bamstats2report.combine.sh
@@ -16,7 +16,7 @@ for file in report*; do
     echo "$file"
     rfile=$dirIn/$file
     c=$(less $rfile | wc -l);
-    if [ "$c" -eq "11" ]; then
+    if [ "$c" -eq "13" ]; then
         less $rfile | awk '{c=c+1; print c"\t"$3}' | sort -k1,1 > y;
         join x y | sed 's/ /\t/g' | sort -k1,1n > $dirOut/summary.xls ;
         rm -rf x y;
