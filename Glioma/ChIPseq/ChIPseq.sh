191c141ec61a0d65d0930b8729bb92e3e296f448
Gloria Li <glorialuolanli@gmail.com>
2016-10-25 14:48:08 -0700

    Add histone modifications global comparison.

--- Glioma/ChIPseq/ChIPseq.sh
+++ Glioma/ChIPseq/ChIPseq.sh
@@ -547,3 +547,23 @@ for wig in $dirIn/*/*.wig.gz; do
 		$JAVA -jar -Xmx10G $RegCov -w $wig -o $dirOut/$mark/ -s hg19 -n $name -bin 500 -step 500
 	fi
 done
+dirIn=/projects/epigenomics2/users/lli/glioma/ChIPseq/global/;
+for mark in H3K27me3 H3K36me3 H3K9me3; do
+	cd $dirIn/$mark/
+	echo -e "Samples\tMark\tType\tmin\tymin\tbottom\tmiddle\ttop\tymax\tmax" > $dirIn/$mark.glioma_NPC.quantile;
+	for file in *.coverage; do
+		less $file | awk '{if($4>0){print $1":"$2"-"$3"\t"$4}}' | sort -k2,2nr > $file.sorted;
+		less $file.sorted | awk '{if(NR<=400){print $0 >> "'$file'"".sorted.top"}else{print $0 >> "'$file'"".sorted.rest"}}';
+	done
+	for f1 in *CEMT*.coverage; do		
+		for f2 in *NPC*.coverage; do
+			s1=$(basename $f1 | cut -d'.' -f4);		
+			s2=$(basename $f2 | cut -d'.' -f4);
+			echo $mark $s1 $s2;			
+			join <(less $f1.sorted.top | sort -k1,1) <(less $f2.sorted.top | sort -k1,1) -t $'\t' | awk '{print $0"\t"$2/$3}' | sort -k4,4n | awk '{s[NR]=$4} END{print "'$s1'"".""'$s2'""\t""'$mark'""\ttop\t"s[1]"\t"s[int(NR/10)]"\t"s[int(NR/4)]"\t"s[int(NR/2)]"\t"s[NR-int(NR/4)]"\t"s[NR-int(NR/10)]"\t"s[NR]}' >> $dirIn/$mark.glioma_NPC.quantile;
+			join <(less $f1.sorted.rest | sort -k1,1) <(less $f2.sorted.rest | sort -k1,1) -t $'\t' | awk '{print $0"\t"$2/$3}' | sort -k4,4n | awk '{s[NR]=$4} END{print "'$s1'"".""'$s2'""\t""'$mark'""\trest\t"s[1]"\t"s[int(NR/10)]"\t"s[int(NR/4)]"\t"s[int(NR/2)]"\t"s[NR-int(NR/4)]"\t"s[NR-int(NR/10)]"\t"s[NR]}' >> $dirIn/$mark.glioma_NPC.quantile;			
+		done
+	done
+done
+
+
