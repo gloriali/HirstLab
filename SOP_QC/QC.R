0649e47bb0b4d130d6e34fa6b6744702348cbcc3
gloriali <glorialuolanli@gmail.com>
2018-01-26 12:23:14 -0800

    Add 3X replicate.

--- SOP_QC/QC.R
+++ SOP_QC/QC.R
@@ -5,6 +5,7 @@ library(plyr)
 library(dplyr)
 library(ggplot2)
 library(gridExtra)
+library(reshape2)
 setwd("/projects/epigenomics2/users/lli/")
 meDIP_qPCR <- read.delim("meDIP_qPCR_primers_hg19.5mC.bed", as.is = T) %>%
   mutate(type = gsub("[0-9]+", "", ID), CpG_density = n/(end-start)*100)
@@ -26,28 +27,30 @@ meDIP_qPCR_CGdensity <- read.delim("meDIP_qPCR_primers_hg19.CpGdensity.bed", as.
 ggsave(meDIP_qPCR_CGdensity_figure, file = "meDIP_qPCR_CGdensity_figure.pdf", height = 4, width = 5)
 
 ## QC for SOP
-setwd("/projects/epigenomics3/bams/hg19/UBC_meDIPQC_01NOV2017/")
-QC_summary <- read.delim("SOP_QC_summary.txt", as.is = T) %>% mutate(Sample = gsub(".*-", "", gsub("_S.*", "", Library)), Type = gsub("IP-", "", gsub("-\\dX.*", "", Library)))
+setwd("/projects/epigenomics3/epigenomics3_results/users/lli/SOP_QC/")
+QC_summary <- read.delim("summary.xls", as.is = T, row.names = 2, head = F) %>% select(-V1) %>% t() %>% as.data.frame() 
+colnames(QC_summary) <- gsub("without_Dups_and_Q_>=_10", "After_Filter", colnames(QC_summary))
+QC_summary <- QC_summary %>% mutate(Library = gsub("meDIP-HL60-B05_S27", "IP-5mC-3X.2_S5", Library), Sample = gsub(".*-", "", gsub("_S.*", "", Library)), Type = gsub("IP-", "", gsub("-\\dX.*", "", Library)))
 QC_summary_reads <- QC_summary %>% select(Sample, Type, Total_Number_Of_Reads, Number_Reads_Aligned, Number_Uniquely_Aligned_Reads_After_Filter) %>% melt(id.var = c("Type", "Sample"))
-(QC_summary_reads_figure <- ggplot(QC_summary_reads, aes(Sample, value/10^6, fill = Sample)) + 
+(QC_summary_reads_figure <- ggplot(QC_summary_reads, aes(Sample, as.numeric(value)/10^6, fill = Sample)) + 
 		geom_bar(stat = "identity") + 
-		facet_grid(variable ~ Type) + 
+		facet_grid(variable ~ Type, scales = "free_x", space = "free_x") + 
 		guides(fill = "none") + 
 		xlab("") + 
 		ylab("No. of million reads") +
 		theme_bw() + 
 		theme(axis.text.x = element_text(angle = 90)))
-ggsave(QC_summary_reads_figure, file = "QC_summary_reads_figure.pdf", height = 8, width = 5)
+ggsave(QC_summary_reads_figure, file = "SOP_QC_summary_reads_figure.pdf", height = 8, width = 4)
 QC_summary_percent <- QC_summary %>% select(Sample, Type, Mapping_Efficiency, Percent_of_dups, Percent_of_Paired_Alignments, Percent_Uniquely_Aligned_Reads_After_Filter) %>% melt(id.var = c("Type", "Sample"))
-(QC_summary_percent_figure <- ggplot(QC_summary_percent, aes(Sample, value, fill = Sample)) + 
+(QC_summary_percent_figure <- ggplot(QC_summary_percent, aes(Sample, as.numeric(value), fill = Sample)) + 
 		geom_bar(stat = "identity") + 
-		facet_grid(variable ~ Type, scales = "free_y") + 
+		facet_grid(variable ~ Type, scales = "free", space = "free_x") + 
 		guides(fill = "none") + 
 		xlab("") + 
 		ylab("Percent of total number of reads") +
 		theme_bw() + 
 		theme(axis.text.x = element_text(angle = 90)))
-ggsave(QC_summary_percent_figure, file = "QC_summary_percent_figure.pdf", height = 10, width = 5)
+ggsave(QC_summary_percent_figure, file = "SOP_QC_summary_percent_figure.pdf", height = 10, width = 4)
 
 ## coverage vs GC content
 GC_coverage_summary <- read.delim("/projects/epigenomics3/bams/hg19/UBC_meDIPQC_01NOV2017/GCcontent.coverage.summary", as.is = T) %>% mutate(antibody = gsub(".*-", "", gsub("_S.*", "", sample)), type = gsub("IP-", "", gsub("-\\dX.*", "", sample)))
@@ -60,7 +63,7 @@ GC_coverage_summary <- read.delim("/projects/epigenomics3/bams/hg19/UBC_meDIPQC_
 ggsave(GC_coverage_summary_figure, file = "/projects/epigenomics3/bams/hg19/UBC_meDIPQC_01NOV2017/GC_coverage_summary_figure.pdf")
 
 ## spike-in coverage
-spikein_coverage <- read.delim("spike_in.coverage", as.is = T) %>% mutate(Sample = gsub(".*-", "", gsub("_S.*", "", Library)), Type = gsub("IP-", "", gsub("-\\dX.*", "", Library)), Genome = revalue(Genome, c("NC_001416.1" = "Lambda", "NC_001604.1" = "T7", "NC_003287.2" = "M13")))
+spikein_coverage <- read.delim("/projects/epigenomics3/bams/hg19/UBC_meDIPQC_01NOV2017/spike_in.coverage", as.is = T) %>% mutate(Library = gsub("meDIP-HL60-B05_S27", "IP-5mC-3X.2_S5", Library), Sample = gsub(".*-", "", gsub("_S.*", "", Library)), Type = gsub("IP-", "", gsub("-\\dX.*", "", Library)), Genome = revalue(Genome, c("NC_001416.1" = "Lambda", "NC_001604.1" = "T7", "NC_003287.2" = "M13")))
 (spikein_coverage_5mC_figure <- ggplot(spikein_coverage %>% filter(Type == "5mC"), aes(Sample, Coverage, fill = Sample)) + 
 		geom_bar(stat = "identity") + 
 		guides(fill = "none") + 
@@ -68,6 +71,7 @@ spikein_coverage <- read.delim("spike_in.coverage", as.is = T) %>% mutate(Sample
 		xlab("") + 
 		ylab("No. of uniquely aligned reads") + 
 		theme_bw())
+ggsave(spikein_coverage_5mC_figure, file = "SOP_spikein_coverage_5mC_figure.pdf", height = 8, width = 3)
 (spikein_coverage_5hmC_figure <- ggplot(spikein_coverage %>% filter(Type == "5hmC"), aes(Sample, Coverage, fill = Sample)) + 
 		geom_bar(stat = "identity") + 
 		guides(fill = "none") + 
