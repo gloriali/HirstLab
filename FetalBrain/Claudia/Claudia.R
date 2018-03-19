4c23e736ada70ea6c114e1546bb3a8ca7a79f133
gloriali <glorialuolanli@gmail.com>
2018-03-19 06:51:09 -0700

    Add BrainSpan data.

--- FetalBrain/Claudia/Claudia.R
+++ FetalBrain/Claudia/Claudia.R
@@ -12,39 +12,49 @@ library(dplyr)
 library(RCircos)
 library(stringr)
 library(scales)
+library(preprocessCore)
 setwd("/projects/epigenomics3/epigenomics3_results/users/lli/Claudia/")
 load("/projects/epigenomics3/epigenomics3_results/users/lli/Claudia/Claudia.Rdata")
 
 ## ------- 450K array --------
-DNAme <- read.csv("DNAme/180212-FB-betas-@lli.csv") 
-DNAme_long <- DNAme %>% select(-starts_with("NA")) %>% melt(id = "ID") %>% mutate(GW = factor(gsub("\\..*", "", variable), levels = c("GW8", "GW9", "GW10", "GW11", "GW12", "GW13", "GW14", "GW17")))
+DNAme <- read.csv("DNAme/180212-FB-betas-@lli.csv", row.names = 1) 
+DNAme_norm <- DNAme %>% as.matrix() %>% normalize.quantiles() %>% as.data.frame()
+colnames(DNAme_norm) <- colnames(DNAme)
+DNAme_norm$ID <- row.names(DNAme)
+DNAme_long <- DNAme_norm %>% select(ID, starts_with("GW")) %>% melt(id = "ID") %>% mutate(GW = factor(gsub("\\..*", "", variable), levels = c("GW8", "GW9", "GW10", "GW11", "GW12", "GW13", "GW14", "GW17")))
 Olig2_450K <- read.delim("DNAme/Olig2.promoter.450K.bed", head = F, as.is = T)
-(Olig2_450K_figure <- ggplot(DNAme_long %>% filter(ID %in% Olig2_450K$V4), aes(GW, value, color = variable)) + 
-		geom_boxplot() + 
+(Olig2_450K_figure <- ggplot(DNAme_long %>% filter(ID %in% Olig2_450K$V4), aes(GW, as.numeric(value), color = variable)) + 
+		geom_point() + 
+		facet_wrap(~ ID) + 
 		guides(color = "none") + 
 		xlab("") + 
 		ylab("beta value") + 
 		ggtitle("Olig2 promoter") + 
-		theme_bw())
-ggsave(Olig2_450K_figure, file = "Olig2_450K_figure.pdf", height = 4, width = 4)
+		theme_bw() + 
+		theme(axis.text.x = element_text(angle = 90)))
+ggsave(Olig2_450K_figure, file = "Olig2_450K_figure.pdf", height = 3, width = 6)
 GW_hyper_450K <- read.delim("DNAme/DMR.HuFNSC02_HuFNSC04.450K.hyper.bed", head = F, as.is = T)
-(GW_hyper_450K_figure <- ggplot(DNAme_long %>% filter(ID %in% intersect(GW_hyper_450K$V4, DNAme[DNAme$GW17.M594-DNAme$GW13.M291>0.001, "ID"])), aes(GW, value, color = variable)) + 
-		geom_boxplot() + 
+(GW_hyper_450K_figure <- ggplot(DNAme_long %>% filter(ID %in% GW_hyper_450K$V4), aes(GW, value, color = variable)) + 
+		geom_point() + 
+		facet_wrap(~ ID) + 
 		guides(color = "none") + 
 		xlab("") + 
 		ylab("beta value") + 
 		ggtitle("GW17 hyper") + 
-		theme_bw())
-ggsave(GW_hyper_450K_figure, file = "GW_hyper_450K_figure.pdf", height = 4, width = 4)
+		theme_bw() +
+		theme(axis.text.x = element_text(angle = 90)))
+ggsave(GW_hyper_450K_figure, file = "GW_hyper_450K_figure.pdf", height = 8, width = 8)
 GW_hypo_450K <- read.delim("DNAme/DMR.HuFNSC02_HuFNSC04.450K.hypo.bed", head = F, as.is = T)
-(GW_hypo_450K_figure <- ggplot(DNAme_long %>% filter(ID %in% intersect(GW_hypo_450K$V4, DNAme[DNAme$GW17.M594-DNAme$GW13.M291<0.001, "ID"])), aes(GW, value, color = variable)) + 
-		geom_boxplot() + 
+(GW_hypo_450K_figure <- ggplot(DNAme_long %>% filter(ID %in% intersect(GW_hypo_450K$V4, DNAme_norm[DNAme_norm$GW17.M594-DNAme_norm$GW13.M291 < -0.05, "ID"])), aes(GW, value, color = variable)) + 
+		geom_point() + 
+		facet_wrap(~ID) +
 		guides(color = "none") + 
 		xlab("") + 
 		ylab("beta value") + 
 		ggtitle("GW17 hypo") + 
-		theme_bw())
-ggsave(GW_hypo_450K_figure, file = "GW_hypo_450K_figure.pdf", height = 4, width = 4)
+		theme_bw() + 
+		theme(axis.text.x = element_text(angle = 90)))
+ggsave(GW_hypo_450K_figure, file = "GW_hypo_450K_figure.pdf", height = 20, width = 20)
 DNAme$sd <- apply(DNAme[, 2:ncol(DNAme)], 1, sd)
 (GW_450K_figure <- ggplot(DNAme_long %>% filter(ID %in% DNAme[DNAme$sd>quantile(DNAme$sd, 0.8), "ID"]), aes(GW, value, color = variable)) + 
 		geom_boxplot() + 
@@ -64,17 +74,20 @@ RPKM_long <- RPKM %>% melt(id = "ENSG") %>% mutate(GW = factor(gsub("\\..*", "",
 		xlab("") + 
 		ylab("RPKM") + 
 		ggtitle("Olig2") + 
-		theme_bw())
+		theme_bw() +
+		theme(axis.text.x = element_text(angle = 90)))
 ggsave(Olig2_RPKM_figure, file = "Olig2_RPKM_figure.pdf", height = 4, width = 4)
 GW_17_13_DN <- read.delim("/projects/epigenomics/users/lli/FetalBrain/GW/DE/GW_17_13_DN_duplicated.txt", as.is = T, head = F)
-(GW_17_13_DN_figure <- ggplot(RPKM_long %>% filter(ENSG %in% GW_17_13_DN$V1), aes(GW, log10(value), color = variable)) + 
-		geom_boxplot() + 
+(GW_17_13_DN_figure <- ggplot(RPKM_long %>% filter(ENSG %in% intersect(GW_17_13_DN$V1, RPKM[RPKM$GW17.M594-RPKM$GW13.M318 < -5, "ENSG"])), aes(GW, log10(value), color = variable)) + 
+		geom_point() + 
+		facet_wrap(~ENSG) + 
 		guides(color = "none") + 
 		xlab("") + 
 		ylab("log10 RPKM") + 
 		ggtitle("GW17 DN") + 
-		theme_bw())
-ggsave(GW_17_13_DN_figure, file = "GW_17_13_DN_figure.pdf", height = 4, width = 4)
+		theme_bw() + 
+		theme(axis.text.x = element_text(angle = 90)))
+ggsave(GW_17_13_DN_figure, file = "GW_17_13_DN_figure.pdf", height = 8, width = 8)
 GW_17_13_UP <- read.delim("/projects/epigenomics/users/lli/FetalBrain/GW/DE/GW_17_13_UP_duplicated.txt", as.is = T, head = F)
 (GW_17_13_UP_figure <- ggplot(RPKM_long %>% filter(ENSG %in% intersect(GW_17_13_UP$V1, RPKM[RPKM$GW17.M594-RPKM$GW13.M318>1, "ENSG"])), aes(GW, log10(value), color = variable)) + 
 		geom_boxplot() + 
@@ -82,7 +95,8 @@ GW_17_13_UP <- read.delim("/projects/epigenomics/users/lli/FetalBrain/GW/DE/GW_1
 		xlab("") + 
 		ylab("log10 RPKM") + 
 		ggtitle("GW17 UP") + 
-		theme_bw())
+		theme_bw() +
+		theme(axis.text.x = element_text(angle = 90)))
 ggsave(GW_17_13_UP_figure, file = "GW_17_13_UP_figure.pdf", height = 4, width = 4)
 RPKM$sd <- apply(RPKM[, 2:ncol(RPKM)], 1, sd)
 (GW_RPKM_figure <- ggplot(RPKM_long %>% filter(ENSG %in% RPKM[RPKM$sd>quantile(RPKM$sd, 0.8), "ENSG"]), aes(GW, value, color = variable)) + 
@@ -94,5 +108,36 @@ RPKM$sd <- apply(RPKM[, 2:ncol(RPKM)], 1, sd)
 		ggtitle("Top 20% most variable genes") + 
 		theme_bw())
 ggsave(GW_RPKM_figure, file = "GW_RPKM_figure.pdf", height = 4, width = 4)
+BrainSpan <- read.csv("./BrainSpan/expression_matrix.csv", head = F, row.names = 1)
+row_metadata <- read.csv("./BrainSpan/rows_metadata.csv")
+rownames(BrainSpan) <- row_metadata$ensembl_gene_id
+col_metadata <- read.csv("./BrainSpan/columns_metadata.csv") %>% mutate(ID = paste0(donor_name, "_", age, "_", structure_acronym))
+colnames(BrainSpan) <- col_metadata$ID
+BrainSpan$ID <- rownames(BrainSpan)
+BrainSpan_long <- BrainSpan %>% select(ID, contains("pcw")) %>% melt(id = "ID") %>% mutate(donor = gsub("_.*", "", variable), structure = gsub(".*_", "", variable), GW = as.numeric(gsub(".*_", "", gsub(" pcw.*", "", variable))) + 2) %>%
+	filter(GW < 20, !(structure %in% c("CB", "CBC", "CGE", "LGE", "MGE", "DTH", "M1C", "M1C-S1C", "STR", "MD", "HIP", "Ocx", "PCx", "S1C", "TCx", "URL")))
+BrainSpan$GW18mean <- BrainSpan %>% select(contains("16 pcw")) %>% rowMeans()
+BrainSpan$GW14mean <- BrainSpan %>% select(contains("12 pcw")) %>% rowMeans()
+(BrainSpan_GW_17_13_DN_figure <- ggplot(BrainSpan_long %>% filter(ID %in% intersect(GW_17_13_DN$V1, BrainSpan[BrainSpan$GW14mean - BrainSpan$GW18mean > 1, "ID"])), aes(as.character(GW), log10(value), color = variable)) + 
+		geom_point() + 
+		facet_grid(ID ~ structure) + 
+		guides(color = "none") + 
+		xlab("GW") + 
+		ylab("log10 RPKM") + 
+		ggtitle("GW17 DN") + 
+		theme_bw() + 
+		theme(axis.text.x = element_text(angle = 90)))
+ggsave(BrainSpan_GW_17_13_DN_figure, file = "BrainSpan_GW_17_13_DN_figure.pdf", height = 20, width = 20)
+(BrainSpan_GW_17_13_UP_figure <- ggplot(BrainSpan_long %>% filter(ID %in% intersect(GW_17_13_UP$V1, BrainSpan[BrainSpan$GW18mean - BrainSpan$GW14mean > 5, "ID"])), aes(as.character(GW), log10(value), color = variable)) + 
+		geom_boxplot() + 
+		facet_wrap(~ structure) + 
+		guides(color = "none") + 
+		xlab("GW") + 
+		ylab("log10 RPKM") + 
+		ggtitle("GW17 UP") + 
+		theme_bw() + 
+		theme(axis.text.x = element_text(angle = 90)))
+ggsave(BrainSpan_GW_17_13_UP_figure, file = "BrainSpan_GW_17_13_UP_figure.pdf", height = 6, width = 7)
+
 
 save(list = ls(pattern = "figure"), file = "/projects/epigenomics3/epigenomics3_results/users/lli/Claudia/Claudia.Rdata")
