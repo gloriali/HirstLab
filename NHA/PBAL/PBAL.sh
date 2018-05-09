d0b28aae9bce34ba1a10f1c0fbc1b7784eb43019
Gloria Li <glorialuolanli@gmail.com>
2018-05-09 14:55:21 -0700

    compare global methylation to AML patients.

--- NHA/PBAL/PBAL.sh
+++ NHA/PBAL/PBAL.sh
@@ -190,6 +190,47 @@ done
 echo -e $header | cat - x > matrix_genome.5mC
 echo -e $header | cat - a > matrix_CGI.5mC
 rm x a
+## compare to AML samples
+function combine {
+    file=$1
+    dirIn=/projects/epigenomics3/epigenomics3_results/users/lli/AML/
+    lib=$(echo $file | sed -e 's/.CpG.txt.gz//g')
+    /home/lli/HirstLab/Pipeline/shell/WGBS.combine.sh -i $dirIn -o $dirIn -f $file -n $lib 
+}
+export -f combine
+cd /projects/epigenomics3/epigenomics3_results/users/lli/AML/
+ls *.CpG.txt.gz > List.txt
+cat List.txt | parallel --gnu combine 
+BEDTOOLS=/gsc/software/linux-x86_64-centos5/bedtools/bedtools-2.25.0/bin/
+dirIn=/projects/epigenomics3/epigenomics3_results/users/lli/AML/
+echo -e "sample\tcoverage\tN" > $dirIn/qc_5mC_coverage.txt
+echo -e "sample\ttype\tfractional\tN" > $dirIn/qc_5mC_profile.txt 
+echo -e "sample\ttype\tmin\tymin\tlower\tmedian\tupper\tymax\tmax" > $dirIn/qc_5mC_quantile.txt #ymin: 10% quantile; ymax: 90% quantile
+cd $dirIn
+for file in *.combine.5mC.CpG; do
+    lib=$(echo $file | sed -e 's/.combine.5mC.CpG//g')
+    echo "Processing" $lib
+    less $file | awk '{c = $4 + $5; if(c >= 5000){s[5001]++} else {s[c]++}} END{for(i = 3; i <= 5001; i++){print "'$lib'""\t"i"\t"s[i]}}' >> $dirIn/qc_5mC_coverage.txt
+    less $file | awk '{s[int($6*100)]++} END{for(i = 0; i<=100; i++){print "'$lib'""\tgenome\t"i/100"\t"s[i]}}' >> $dirIn/qc_5mC_profile.txt 
+    less $file | awk '{gsub("chr", ""); print $1"\t"$2"\t"$3"\t"$1":"$2"\t"$4"\t"$5}' | $BEDTOOLS/intersectBed -a stdin -b /home/lli/hg19/CGI.forProfiles.BED -wa -wb | awk '{t[$10]=t[$10]+$5; c[$10]=c[$10]+$6} END{for(i in c){print c[i]/(c[i]+t[i])}}' | awk '{s[int($1*100)]++} END{for(i = 0; i<=100; i++){print "'$lib'""\tCGI\t"i/100"\t"s[i]}}' >> $dirIn/qc_5mC_profile.txt 
+    less $file | awk '{print $6}' | sort -k1,1n | awk '{mC[NR]=$1} END{print "'$lib'""\tgenome\t"mC[1]"\t"mC[int(NR/10)]"\t"mC[int(NR/4)]"\t"mC[int(NR/2)]"\t"mC[NR-int(NR/4)]"\t"mC[NR-int(NR/10)]"\t"mC[NR]}' >> $dirIn/qc_5mC_quantile.txt
+    less $file | awk '{gsub("chr", ""); print $1"\t"$2"\t"$3"\t"$1":"$2"\t"$4"\t"$5}' | $BEDTOOLS/intersectBed -a stdin -b /home/lli/hg19/CGI.forProfiles.BED -wa -wb | awk '{t[$10]=t[$10]+$5; c[$10]=c[$10]+$6} END{for(i in c){print c[i]/(c[i]+t[i])}}' | sort -k1,1n | awk '{mC[NR]=$1} END{print "'$lib'""\tCGI\t"mC[1]"\t"mC[int(NR/10)]"\t"mC[int(NR/4)]"\t"mC[int(NR/2)]"\t"mC[NR-int(NR/4)]"\t"mC[NR-int(NR/10)]"\t"mC[NR]}' >> $dirIn/qc_5mC_quantile.txt
+done
+less /home/lli/hg19/CG.strand | awk '{if(NR%2){print $2}}' | sort > x
+less /home/lli/hg19/CGI.forProfiles.BED | awk '{print $4}' | sort > a
+header="ID"
+for file in *combine.5mC.CpG; do
+    sample=$(echo $file | sed -e 's/.combine.5mC.CpG//g')
+    header=$header" "$sample
+    echo -e $sample
+    less $file | awk '{print $1":"$2"-"$3"\t"$6}' | sort -k1,1 | join x - > y
+    mv y x
+    less $file | $BEDTOOLS/intersectBed -a /home/lli/hg19/CGI.forProfiles.BED -b stdin -wa -wb | awk '{t[$4]=t[$4]+$8; c[$4]=c[$4]+$9} END{for(i in t){if(t[i]+c[i]>0){print i"\t"c[i]/(c[i]+t[i])}}}' | sort -k1,1 | join a - > b
+    mv b a
+done
+echo -e $header | cat - x > matrix_genome.5mC
+echo -e $header | cat - a > matrix_CGI.5mC
+rm x a
 
 # DMRs 
 BEDTOOLS=/gsc/software/linux-x86_64-centos5/bedtools/bedtools-2.25.0/bin/
